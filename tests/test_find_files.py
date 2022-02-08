552b83672c3d0f32bdbe15324b4573ffa294b9dd
LimaBD <bdl1998@hotmail.com>
2022-02-08 02:50:42 -0300

    changed tests

--- tests/test_find_files.py
+++ tests/test_find_files.py
@@ -8,6 +8,7 @@
 
 # Standart packages
 import unittest
+import re
 
 
 # Local packages
@@ -24,35 +25,46 @@ class TestIt(TestWrapper):
         """Test for find_files option"""
 
         # Setup test folders and files
-        foldernames = ['main_folder', 'main_folder/another_folder']
+        test_folder = gh.run('echo test-find-files-$$')
+        foldernames = [test_folder, f'{test_folder}/another_folder']
         filenames  = ['arrows.bash', 'numeric.py', 'calc_entropy.c']
-
-        gh.run(f'rm -rf /tmp/{foldernames[0]}')
 
         for folder in foldernames:
             gh.run(f'mkdir /tmp/{folder}')
             for filename in filenames:
                 gh.run(f'touch /tmp/{folder}/{filename}')
-    
-        # Run twice to test the backup folder
-        for i in range(2):
-            gh.run(f'python {self.script_module} --find-files --path "/tmp/{foldernames[0]}"')
 
-        # Check for created files
-        file_list = gh.run(f'ls -R /tmp/{foldernames[0]}')
-        self.assertTrue('backup' in file_list)
-        self.assertTrue('ls-R.list' in file_list)
-        self.assertTrue('ls-alR.list' in file_list)
-		# TODO: self.assertTrue('ls-R.list.log'in file_list)
-        # TODO: self.assertTrue('ls-alR.list.log' in file_list)
+        # Run twice to test the backup folder
+        result = ''
+        for _ in range(2):
+            result = gh.run(f'python {self.script_module} --find-files --path "/tmp/{test_folder}"').split('\n')
+
+        # Check output
+        self.assertEqual(len(result), 4)
+
+        for line in result:
+            self.assertTrue(bool(re.search(r"[drwx-]+\s+\d+\s+\w+\s+\w+\s+\d+\s+\w+\s+\d+\s+\d\d:\d\d\s+[\w/]+", line)))
+
+        # Setup expected new files
+        new_long_file  = 'ls-R.list'
+        new_short_file = 'ls-alR.list'
+
+        expected_new_files  = [new_long_file, new_short_file]
+        expected_new_files += [new_file + '.log'         for new_file in expected_new_files]
+        expected_new_files += ['backup' + new_file + '.' for new_file in expected_new_files]
+
+        # Check if new files exist
+        file_list = gh.run(f'ls -R /tmp/{test_folder}')
+
+        for expected_file in expected_new_files:
+            expected_file = expected_file.replace('backup/', '')
+            self.assertTrue(filename in file_list)
 
         # Check content of created files
-        # TODO: change number of words when the .log files are implemented
-        self.assertEqual(gh.run(f'cat /tmp/{foldernames[0]}/ls-R.list | wc -m'), '350')
-        self.assertEqual(gh.run(f'cat /tmp/{foldernames[0]}/backup/ls-R.list.* | wc -m'), '255')
-        # TODO: check why drwxrwxr-x 0 on start of the file
-		# self.assertEqual(gh.run(f'cat /tmp/{foldernames[0]}/ls-alR.list | wc -m'), '992')
-        # self.assertEqual(gh.run(f'cat /tmp/{foldernames[0]}/backup/ls-alR.list.* | wc -m'), '703')
+        for new_file in expected_new_files:
+            if '.log' not in new_file:
+                for line in gh.run(f'cat /tmp/{test_folder}/{filename}'):
+                    self.assertTrue(bool(re.search(r"[drwx-]+\s+\d+\s+\w+\s+\w+\s+\d+\s+\w+\s+\d+\s+\d\d:\d\d\s+[\w/]+", line)))
 
 
 if __name__ == '__main__':
