57bdad54a4f834736caf3320b3644749a49e1379
Tom O'Hara <tomasohara@gmail.com>
2022-02-16 00:10:31 -0600

    make --ignore cumulative

--- compare-log-output.sh
+++ compare-log-output.sh
@@ -1,6 +1,6 @@
 #! /bin/sh
 #
-# compare-tmr-output.sh: compare trace output listings ignoring differences
+# compare-log-output.sh: compare trace output listings ignoring differences
 # due to timestamps
 #
 # TODO:
@@ -17,11 +17,13 @@
 # Parse command-line arguments
 diff=kdiff.sh
 ## OLD: ignore='(0x)?[0-9A-Fa-f]{7,8}'
-ignore='(0x)?[0-9A-Fa-f]{7,16}'
+## OLD2: ignore='(0x)?[0-9A-Fa-f]{7,16}'
+ignore='((0x)?[0-9A-Fa-f]{7,16})'
 moreoptions=0; case "$1" in -*) moreoptions=1 ;; esac
 while [ "$moreoptions" = "1" ]; do
     if [ "$1" = "--trace" ]; then
 	set -o xtrace
+	## DEBUG: set -o verbose
     elif [ "$1" = "--diff" ]; then
 	diff="$2";
 	shift
@@ -29,8 +31,11 @@ while [ "$moreoptions" = "1" ]; do
 	diff="diff";
     elif [ "$1" = "--include-ptrs" ]; then
         ignore="";
+    elif [ "$1" = "--reset-ignore" ]; then
+        ignore="";
     elif [ "$1" = "--ignore" ]; then
-	ignore="$2";
+	## OLD: ignore="$2";
+	ignore="$ignore|($2)";
 	shift
     else
 	echo "unknown option: $1";
@@ -45,7 +50,7 @@ if [ "$1" = "" ]; then
     echo ""
     echo "Usage: $script_name [options] file1 file2-or-dir"
     echo ""
-    echo "    options: [--ignore perl-regex] [--include-ptrs] [--plain-diff] [--diff program]"
+    echo "    options: [--ignore perl-regex] [--include-ptrs | --reset-ignore] [--plain-diff] [--diff program]"
     echo ""
     echo "Examples:"
     echo ""
@@ -57,7 +62,9 @@ if [ "$1" = "" ]; then
     echo "- The regex pattern matching is not case sensitive (i.g., case ignored)."
     echo "- Timestamps are removed prior to comparison."
     echo "- Hex addresses are removed as with '--ignore [0-9A-Fa-f]{7,16}'."
-    echo "- Additional filtering can be done via --ignore, but only one can be given (implies --include-ptrs)."
+    echo "- Additional filter pattern can be scpeified via --ignore".
+    echo "- Use --reset-ignore to discard existing patterns."
+    echo "- The --include-ptrs option is alias for --reset-ignore."
     echo "- kdiff is default diff program (see kdiff.sh)."
     echo "- The pattern for ignore is a perl regex."
     echo "- Only one pattern is allowed, including the hex address filter."
@@ -93,7 +100,8 @@ perl -pe "s/$timestamp_regex1//ig; s/$timestamp_regex2//ig;" "$file2" > "/tmp/$b
 # Strip out user-specified patterns
 # Note: include hex address stripping
 if [ "$ignore" != "" ]; then
-    perl -i.bak -pe "s/$ignore//gi;" "/tmp/$base1" "/tmp/$base2"
+    ## OLD: perl -i.bak -pe "s/$ignore//gi;" "/tmp/$base1" "/tmp/$base2"
+    perl -i.bak -pe "s@($ignore)@@gi;" "/tmp/$base1" "/tmp/$base2"
 fi
 
 # Do comparison of the result
