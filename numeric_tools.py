eee3901e258a9356ec400f82bdccb20bbd536a08
Tom O'Hara <tomasohara@gmail.com>
2022-02-18 23:31:38 -0600

    comments for Bruno; low-priority for now

--- numeric_tools.py
+++ numeric_tools.py
@@ -4,7 +4,33 @@
 # This can be used as command-line tool or as module.
 #
 # TODO: sanitization of functions input.
-
+#
+## Tom: here are some comments
+## - These are low priority so just review them briefly to keeo in mind for future scripts.
+##   Right now the priority is getting the alias test suite in place.
+##
+## Tom: Useful to show examples from revised tomohara-aliases.bash (e.g., in header comments)
+##
+## function hex2dec { numeric_tools.py --hex2dec --i $1; }
+## function dec2hex { numeric_tools.py --dec2hex --i $1; }
+## function bin2dec { numeric_tools.py --bin2dec --i $1; }
+## function dec2bin { numeric_tools.py --dec2bin --i $1; }
+## function comma-ize-number () { python numeric_tools.py --comma-ize; }
+## function apply-numeric-suffixes () { numeric_tools.py --suffix; }
+## function apply-usage-numeric-suffixes () { numeric_tools.py --usage-suffix; }
+##
+## Tom: The following is failing
+##
+##   $ echo 1234 987654321 | comma-ize-number 
+##   1,234 987,654,321
+##
+##   current output:
+##     1,234 987654,321
+##
+## Tom: add function tracing throughout
+##    ex: debug.trace(5, f"hex2dec({number})")
+## This helps when determining flow of control.
+##
 
 """
 This script groups several tools to deal with numerical systems and more.
@@ -12,6 +38,7 @@ This script groups several tools to deal with numerical systems and more.
 ex:
 \t$ python numeric_tools.py --dec2bin 20
 """
+## Tom (missing --i above): --dec2bin --i 20
 
 
 # Standard packages
@@ -21,10 +48,14 @@ import sys
 
 # Local packages
 from mezcla.main import Main
+from mezcla      import debug
 from mezcla      import system
 
 
 # Command-line labels constants
+## Tom: good to have --input w/ --i as alias (i.e., general only use one letter
+## switches as abbreviations). Note that GNU's convention uses dashes for long
+## names and -'s for single letter ones), so --i is a little confusing.
 INPUT_SHORT       = 'i'
 HEX2DEC           = 'hex2dec'
 DEC2HEX           = 'dec2hex'
@@ -38,9 +69,12 @@ COMMA             = 'comma-ize'
 
 class NumericSystem(Main):
     """This script groups several tools to deal with numerical systems and more"""
+    ## Tom: misleading comment as really just argument processing
+    ## suggestion: """Argument processing class""
 
 
     ## class-level member variables for arguments (avoids need for class constructor)
+    ## Tom: reserve '##' for temporary comments. I used this in template.py for TODO notes.
     input_string        = ''
     hex2dec             = False
     dec2hex             = False
@@ -65,13 +99,18 @@ class NumericSystem(Main):
         self.suffixes_first_only = self.has_parsed_option(SUFFIX_FIRST_ONLY)
         self.usage_suffix        = self.has_parsed_option(USAGE_SUFFIX)
         self.comma               = self.has_parsed_option(COMMA)
-
+        ## Tom: good to ensure just one of above used
+        ##    debug.assertion((0 < sum([self.hex2dec, ..., self.comma]) <= 1), "Options are mutually exclusive")}
+        ## Tom: trace instance after args parsed
+        debug.trace_object(5, self, label="Script instance")
 
     def run_main_step(self):
         """Process input stream"""
 
         # Read stdin if not positional input provided
         if not self.input_string and not sys.stdin.isatty():
+            ## Tom: use all of stdin (not just first line)
+            ## Also, add a function for tracing input as TL.VERBOSE (e.g., system.read_all_stdin)
             self.input_string = self.input_stream.readlines()[0]
 
 
@@ -91,13 +130,16 @@ class NumericSystem(Main):
             result = apply_usage_suffixes(self.input_string)
         elif self.comma:
             result = comma_ize_number(self.input_string)
-
+        ## Tom: always good to report unexpected conditions
+        else:
+            raise NotImplementedError()
 
         print(result)
 
 
 def hex2dec(number):
     """Convert hexadecimal to decimal"""
+    ## Tom: debug.trace(5, f"hex2dec({number})")
     return int(str(number), 16)
 
 
@@ -118,6 +160,8 @@ def dec2bin(number):
 
 def comma_ize_number(number):
     """Add commas to number"""
+    ## Tom: probabily need a loop
+    ##   perl -pe 'while (/\d\d\d\d/) { s/(\d)(\d\d\d)([^\d])/\1,\2\3/g; } ';
     return re.sub(r'(\d)(\d\d\d)([^\d])', r'\1,\2\3', str(number))
 
 
@@ -146,9 +190,12 @@ def format_bytes(size, precision=1):
 # EX: apply_suffixes("1024, 1572864, 1073741824", first_only=True) => "1K 1572864 1073741824"
 def apply_suffixes(numbers, first_only=False, precision=1):
     """Convert number to use K/M/G suffixes"""
+    ## Tom: mention arguments (e.g., """Apply K/M/G/T suffixes to NUMBERS, optionally just FIRST_ONLY and using PRECISION""")
 
     # Process entered string
+    ## Tom: use numbers.split(); also rename to something like numeric_text as it would seem to be a list
     size_list = numbers.split(' ')
+    
 
     # Calculate power labels and append results
     result = ''
