535d17c0be041b3152cbc6d409827afbd4f367dd
LimaBD <bdl1998@hotmail.com>
2022-02-18 21:38:17 -0300

    added tests examples for batspp

---
+++ tests/batspp_example.bash
@@ -0,0 +1,50 @@
+# Example tests using BATSPP.
+#
+# Usage: $ batspp batspp_example.bash
+#
+
+
+# You can run tests using the command line 
+# With '$ [command]' followed by the expected output.
+#
+# $ echo -e "hello\nworld"
+# hello
+# world
+#
+# $ echo "this is a test" | wc -c
+# 15
+
+
+# Also you can test bash functions with
+# using [functions + args] => [expected]
+#
+# This test should work fine:
+# fibonacci 9 => "0 1 1 2 3 5 8 13 21 34"
+#
+# This test should fail:
+# fibonacci 3 => "8 2 45 34 3 5"
+#
+function fibonacci () {
+    result=""
+
+    a=0
+    b=1
+    for (( i=0; i<=$1; i++ ))
+    do
+        result="$result$a "
+        fn=$((a + b))
+        a=$b
+        b=$fn
+    done
+
+    echo $result
+}
+
+
+# And you can test aliases too:
+#
+# $ fibonacci 9
+# The Fibonacci series is:
+# 0 1 1 2 3 5 8 13 21 34
+#
+alias fibonacci='echo "The Fibonacci series is: "; fibonacci'
