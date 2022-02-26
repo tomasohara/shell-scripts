ed8a4a9500d70ddc6833e4759cd8de7ff0ddb163
LimaBD <bdl1998@hotmail.com>
2022-02-26 18:05:34 -0300

    implemented command tests and changed assertion

--- batspp.py
+++ batspp.py
@@ -4,16 +4,14 @@
 #
 # This process and run custom tests using bats-core.
 #
-# Assertions:
-# - assert_equals     [actual] [expected]
-# - assert_not_equals [actual] [expected]
-#
 # NOTE: It is only necessary to have installed bats-core
 #       no need for bats-assertions library.
 #
-# TODO: pretty result.
-#       pretty bats file?.
-#       --save PATH?.
+## TODO: solve test alias.
+##       pretty result.
+##       debug tests.
+##       negative assertion (=/>).
+##       --save PATH for bats file?.
 
 
 """
@@ -21,18 +19,20 @@ BATSPP
 
 This process and run custom tests using bats-core.
 
-Examples
-  test "This is a single line test" { assert_equal $((3+7)) 10; }
+You can run tests for aliases and more using the
+command line with '$ [command]' followed by the
+expected output:
 
-  test "This is a multiline test" {
-  \tfirst_value=9
-  \tsecond_value=6
-  \assert_equals $(($first_value+$second_value)) 15
-  }
+ $ echo -e "hello\nworld"
+ hello
+ world
 
-Assertions:
-  assert_equals     [actual] [expected]
-  assert_not_equals [actual] [expected]
+ $ echo "this is a test" | wc -c
+ 15
+
+Also you can test bash functions:
+ [functions + args] => [expected]:
+ fibonacci 9 => "0 1 1 2 3 5 8 13 21 34"
 """
 
 
@@ -51,19 +51,14 @@ from mezcla      import glue_helpers as gh
 FILENAME = 'filepath'
 
 
-# Tests constants
-EXTENSION         = '.bpp'
-ASSERT_EQUALS     = 'assert_equals'
-ASSERT_NOT_EQUALS = 'assert_not_equals'
-
-
 class Batspp(Main):
     """This process and run custom tests using bats-core"""
 
 
     # Global States
     filename     = ''
-    test_content = ''
+    file_content = ''
+    bats_content = '#!/usr/bin/env bats\n\n'
 
 
     def setup(self):
@@ -71,102 +66,192 @@ class Batspp(Main):
 
         # Check the command-line options
         self.filename     = self.get_parsed_argument(FILENAME, "")
-        self.test_content = system.read_file(self.filename)
+        self.file_content = system.read_file(self.filename)
+
+        debug.trace(7, f'pybats - filename to run tests: {self.filename}')
 
 
     def run_main_step(self):
         """Process main script"""
 
 
-        # Delete comments
-        self.test_content = re.sub(r'#.*\n', '', self.test_content)
+        # Check if finish with newline
+        if not self.file_content.endswith('\n\n'):
+            self.file_content += '\n'
 
 
-        # Convert single line tests to multiline
-        self.test_content = re.sub(r'{\s+', '{\n', self.test_content)
-        self.test_content = re.sub(';', '\n', self.test_content)
+        self.__process_setup()
+        self.__process_teardown()
+        self.__process_tests()
 
 
-        bats_content = '#! /usr/bin/env bats\n\n'
+        # Save test file and run tests
+        gh.write_file(self.temp_file, self.bats_content)
+        debug.trace(7, f'pybats - running test {self.temp_file}')
+        print(gh.run(f'bats {self.temp_file}'))
 
 
-        # Process setup
-        test_absolute_dir = gh.dir_path(gh.real_path(self.filename))
-
-        bats_content +=  'setup() {\n'
-        bats_content += f'DIR="{test_absolute_dir}/../:$PATH"\n'
-
-        setup_block   = re.match(r'setup\(\)\s+{([^}]+)', self.test_content)
-        bats_content += setup_block.group(1) if setup_block else ''
-
-        bats_content +=  '}\n'
+    def __process_setup(self):
+        """Process tests setup"""
+        debug.trace(7, f'pybats - processing setup')
 
 
-        # Process tests
-        # TODO: refactor groups, remove 'extra'
-		#       solve problem comparing strings.
-        #       assert_success.
-        #       assert_failure.
-        #       assert_output.
-        #       refute_output.
-        tests   = re.findall(r'test\s+"(.*)"\s+{((.|\n)[^}]+)', self.test_content)
-        asserts = [Assert(f'({ASSERT_EQUALS})\\s+([^\\s]+)\\s+([^\n\\s]+)', r'[ \2 -eq \3 ]'),
-                   Assert(f'({ASSERT_NOT_EQUALS})\\s+([^\\s]+)\\s+([^\n\\s]+)', r'[ \2 -ne \3 ]')]
-
-        for test_text, test_content, extra in tests:
-
-            # Append test header
-            bats_content += '@test "' + test_text + '" {'
-
-            # Append test lines
-            for line in test_content.split('\n'):
-
-                assertion_index = [i for i, assertion in enumerate(asserts) if assertion.check_assertion(line)]
-                bats_content   += asserts[assertion_index[0]].get_bats_assertion(line) if assertion_index else line
-                bats_content   += '\n'
-
-            bats_content += '}\n'
+        self.bats_content +=  'setup() {\n'
 
 
-        # TODO: Process teardown
+        # Make executables ./tests/../ visible to PATH
+        #
+        # This is usefull then the file is specially for testing.
+        #
+        # The structure should be:
+        #  project
+        #     ├ script.bash
+        #  	  └ tests/test_script.bpp
+        ## TODO: add optional target dir.
+        self.bats_content += ('\t# Make executables ./tests/../ visible to PATH\n'
+                              f'\tDIR="{gh.dir_path(gh.real_path(self.filename))}/../:$PATH"\n')
 
 
-        # Get filepath
-        bats_filename = gh.run(f'echo {gh.basename(self.filename, EXTENSION)}-$$.bats')
-        bats_filepath = f'/tmp/{bats_filename}'
+        # Load file
+        self.bats_content += ('\n\t# Load file\n'
+                              '\tshopt -s expand_aliases\n'
+                              f'\tsource {gh.real_path(self.filename)}\n'
+                              '}\n\n')
 
 
-        # Save and run tests
-        gh.write_file(bats_filepath, bats_content)
-        gh.run(f'chmod +x {bats_filepath}')
-        debug.trace(debug.USUAL, f'pybats - running test {bats_filepath}')
-        print(gh.run(f'{bats_filepath}'))
+    def __process_teardown(self):
+        """Process teardown"""
+        debug.trace(7, f'pybats - processing teardown')
+        # WORK-IN-PROGRESS
 
 
-class Assert:
-    """Class Assert"""
+    def __process_tests(self):
+        """Process tests"""
+        debug.trace(7, f'pybats - processing tests')
 
 
-    # Globals
-    assert_pattern = ''
-    bats_pattern   = ''
+        # Setup patterns
+        # NOTE: repeated patterns code but improves readability.
+        indent_pattern = r'^[^\w\$\(\n\{\}]'
+
+        command_test_pattern = (fr'({indent_pattern}*)' # indent
+                                r'\$\s+(.+?)\n'         # command
+                                r'(.+?)\n'              # expected output
+                                fr'{indent_pattern}*$') # blank indent (end test)
+        debug.trace(7, f'batspp - pattern for command tests: {command_test_pattern}')
+
+        function_test_pattern = (fr'({indent_pattern}*)'   # indent
+                                 r'(.+?)'                  # functions + args
+                                 r'\s+=>\s+'               # assert equals
+                                 r'((.|\n)+?)'             # expected output
+                                 fr'\n{indent_pattern}*$') # blank indent (end test)
+        debug.trace(7, f'batspp - pattern for function tests: {function_test_pattern}')
 
 
-    def __init__(self, assert_pattern, bats_pattern):
-        self.assert_pattern = assert_pattern
-        self.bats_pattern   = bats_pattern
+        # Extract tests
+        command_tests  = CommandTestsExtractor(command_test_pattern, re.DOTALL | re.MULTILINE)
+        function_tests = FunctionTestsExtractor(function_test_pattern, re.MULTILINE)
+
+        tests  = []
+        tests += command_tests.get_tests(self.file_content)
+        tests += function_tests.get_tests(self.file_content)
 
 
-    def check_assertion(self, assert_text):
-        """Retuns if an assertion corresponds to this assert"""
-        return bool(re.search(self.assert_pattern, assert_text))
+        # Append tests
+        for actual, expected in tests:
+            self.bats_content += (f'@test {repr(actual)[:-1]} should return {repr(expected)[1:]} {{\n'
+                                  f'\tactual=$({actual})\n'
+                                  f'\texpected=${repr(expected)}\n'
+                                  f'\t[ "$actual" == "$expected" ]\n'
+                                  '}\n\n')
 
 
-    def get_bats_assertion(self, assert_text):
-        """Convert assertion to bats-core style"""
-        result = re.sub(self.assert_pattern, self.bats_pattern, assert_text)
-        debug.trace(debug.USUAL, f'assert.get_bats_assertion() - replaced {assert_text} by {result}')
-        return result
+class TestsExtractor:
+    """Class TestsExtractor for extract and process tests"""
+
+
+    def __init__(self, pattern, re_flags):
+        self._pattern    = pattern
+        self._re_flags   = re_flags
+        self._tests_list = []
+
+
+    def _extract_from_text(self, text):
+        """Extract tests from text"""
+
+        self._tests_list = re.findall(self._pattern, text, flags=self._re_flags)
+
+        # Ensure that there is only 3 groups per test
+        for i, test in enumerate(self._tests_list):
+            #                      indent   actual  expected
+            self._tests_list[i] = (test[0], test[1], test[2])
+
+        debug.trace(7, f'pybats - tests founded: {self._tests_list}')
+
+
+    def _remove_indent(self):
+        """Remove indent of assertion"""
+        for i, test in enumerate(self._tests_list):
+            indent, command, expected = test
+            self._tests_list[i] = (command, re.sub(indent, '', expected))
+
+        debug.trace(7, f'pybats - indent removed from tests: {self._tests_list}')
+
+
+    def _sanitize(self):
+        """Sanitize tests"""
+        for i, test in enumerate(self._tests_list):
+            command, expected = test
+
+            # Strip whitespaces
+            command  = command.strip()
+            expected = expected.strip()
+
+            # Remove initial and trailing quotes
+            command = re.sub(f'^(\"|\')(.*)(\"|\')$', r'\2', command)
+            expected = re.sub(f'^(\"|\')(.*)(\"|\')$', r'\2', expected)
+
+            self._tests_list[i] = (command, expected)
+
+        debug.trace(7, f'pybats - sanitized tests: {self._tests_list}')
+
+
+    def get_tests(self, text):
+        """Extract and process tests from text"""
+        self._extract_from_text(text)
+        self._remove_indent()
+        self._sanitize()
+
+        debug.trace(7, f'pybats - final test list: {self._tests_list}')
+        return self._tests_list
+
+
+# NOTE: this is not necessary but it improves readability.
+class CommandTestsExtractor(TestsExtractor):
+    """Class CommandTestsExtractor to extract and process specific command tests"""
+
+
+class FunctionTestsExtractor(TestsExtractor):
+    """Class FunctionTestsExtractor to extract and process specific function tests"""
+
+
+    def _disable_alias(self):
+        """Disable alias adding // in functions tests"""
+        for i, test in enumerate(self._tests_list):
+            command, expected = test
+            self._tests_list[i] = ('\\' + command, expected)
+        debug.trace(7, f'pybats - disabled alias to tests: {self._tests_list}')
+
+
+    def get_tests(self, text):
+        """Extract and process tests from text"""
+        self._extract_from_text(text)
+        self._remove_indent()
+        self._disable_alias()
+        self._sanitize()
+
+        debug.trace(7, f'pybats - final test list: {self._tests_list}')
+        return self._tests_list
 
 
 if __name__ == '__main__':
