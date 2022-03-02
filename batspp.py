340933a2c29138f4deb50de4e124ac7bf7c0bcb0
LimaBD <bdl1998@hotmail.com>
2022-03-02 01:06:54 -0300

    implemented function assert ne; implemented local setup for command tests; renamed TestExtractor and class methods to improve readability; refactor loops in tests classes for less overhead and duplicated code; refactor _sanitize() class method; patterns moved to specific tests classes; updated old filename appareances 'pybats'; added pylint disable to __process_teardown

--- batspp.py
+++ batspp.py
@@ -1,6 +1,6 @@
 #! /usr/bin/env python
 #
-# PYBATS
+# BATSPP
 #
 # This process and run custom tests using bats-core.
 #
@@ -8,8 +8,8 @@
 #       no need for bats-assertions library.
 #
 ## TODO: pretty result.
+##       setup for functions tests.
 ##       debug tests.
-##       negative assertion (=/>).
 ##       --save PATH for bats file?.
 
 
@@ -50,6 +50,10 @@ from mezcla      import glue_helpers as gh
 FILENAME = 'filepath'
 
 
+# Pattern constants
+INDENT_PATTERN = r'^[^\w\$\(\n\{\}]*'
+
+
 class Batspp(Main):
     """This process and run custom tests using bats-core"""
 
@@ -67,7 +71,7 @@ class Batspp(Main):
         self.filename     = self.get_parsed_argument(FILENAME, "")
         self.file_content = system.read_file(self.filename)
 
-        debug.trace(7, f'pybats - filename to run tests: {self.filename}')
+        debug.trace(7, f'batspp - filename to run tests: {self.filename}')
 
 
     def run_main_step(self):
@@ -86,13 +90,13 @@ class Batspp(Main):
 
         # Save test file and run tests
         gh.write_file(self.temp_file, self.bats_content)
-        debug.trace(7, f'pybats - running test {self.temp_file}')
+        debug.trace(7, f'batspp - running test {self.temp_file}')
         print(gh.run(f'bats {self.temp_file}'))
 
 
     def __process_setup(self):
         """Process tests setup"""
-        debug.trace(7, f'pybats - processing setup')
+        debug.trace(7, f'batspp - processing setup')
 
 
         self.bats_content +=  'setup() {\n'
@@ -118,6 +122,7 @@ class Batspp(Main):
                               '}\n\n')
 
 
+    # pylint: disable=no-self-use
     def __process_teardown(self):
         """Process teardown"""
         debug.trace(7, f'pybats - processing teardown')
@@ -126,131 +131,204 @@ class Batspp(Main):
 
     def __process_tests(self):
         """Process tests"""
-        debug.trace(7, f'pybats - processing tests')
+        debug.trace(7, f'batspp - processing tests')
+
+        command_tests  = CommandTests()
+        function_tests = FunctionTests()
+
+        self.bats_content += command_tests.get_bats_tests(self.file_content)
+        self.bats_content += function_tests.get_bats_tests(self.file_content)
 
 
-        # Setup patterns
-        # NOTE: repeated patterns code but improves readability.
-        indent_pattern = r'^[^\w\$\(\n\{\}]'
+class CustomTestsToBats:
+    """Base class to extract and process tests"""
 
-        command_test_pattern = (fr'({indent_pattern}*)' # indent
-                                r'\$\s+(.+?)\n'         # command
-                                r'(.+?)\n'              # expected output
-                                fr'{indent_pattern}*$') # blank indent (end test)
-        debug.trace(7, f'batspp - pattern for command tests: {command_test_pattern}')
-
-        function_test_pattern = (fr'({indent_pattern}*)'   # indent
-                                 r'(.+?)'                  # functions + args
-                                 r'\s+=>\s+'               # assert equals
-                                 r'((.|\n)+?)'             # expected output
-                                 fr'\n{indent_pattern}*$') # blank indent (end test)
-        debug.trace(7, f'batspp - pattern for function tests: {function_test_pattern}')
+    # Debug global class variables
+    _debug_test_number = 0
 
 
-        # Extract tests
-        command_tests  = CommandTestsExtractor(command_test_pattern, re.DOTALL | re.MULTILINE)
-        function_tests = FunctionTestsExtractor(function_test_pattern, re.MULTILINE)
-
-        tests  = []
-        tests += command_tests.get_tests(self.file_content)
-        tests += function_tests.get_tests(self.file_content)
+    def __init__(self, pattern, re_flags=0):
+        self._re_flags      = re_flags
+        self._pattern       = pattern
+        self._assert_equals = True
 
 
-        # Append tests
-        for actual, expected in tests:
-            self.bats_content += (f'@test {repr(actual)[:-1]} should return {repr(expected)[1:]} {{\n'
-                                  f'\tactual=$({actual})\n'
-                                  f'\texpected=${repr(expected)}\n'
-                                  f'\t[ "$actual" == "$expected" ]\n'
-                                  '}\n\n')
+    def _first_process(self, match):
+        """First process after match, format matched results into [indent, setup, actual, expected]"""
+
+        # NOTE: this must be overriden.
+        # NOTE: the returned values must be like:
+        result = ['indent', 'setup', 'actual', 'expected']
+
+        debug.trace(7, f'batspp (test {self._debug_test_number}) - _first_process({match}) => {result}')
+        return result
 
 
-class TestsExtractor:
-    """Class TestsExtractor for extract and process tests"""
+    def _sanitize(self, test):
+        """Remove indent and sanitize test fields"""
 
+        result = [test[0]]
 
-    def __init__(self, pattern, re_flags):
-        self._pattern    = pattern
-        self._re_flags   = re_flags
-        self._tests_list = []
+        for field in test[1:]:
 
-
-    def _extract_from_text(self, text):
-        """Extract tests from text"""
-
-        self._tests_list = re.findall(self._pattern, text, flags=self._re_flags)
-
-        # Ensure that there is only 3 groups per test
-        for i, test in enumerate(self._tests_list):
-            #                      indent   actual  expected
-            self._tests_list[i] = (test[0], test[1], test[2])
-
-        debug.trace(7, f'pybats - tests founded: {self._tests_list}')
-
-
-    def _remove_indent(self):
-        """Remove indent of assertion"""
-        for i, test in enumerate(self._tests_list):
-            indent, command, expected = test
-            self._tests_list[i] = (command, re.sub(indent, '', expected))
-
-        debug.trace(7, f'pybats - indent removed from tests: {self._tests_list}')
-
-
-    def _sanitize(self):
-        """Sanitize tests"""
-        for i, test in enumerate(self._tests_list):
-            command, expected = test
+            # Remove indent
+            field = re.sub(test[0], '', field)
 
             # Strip whitespaces
-            command  = command.strip()
-            expected = expected.strip()
+            field = field.strip()
 
             # Remove initial and trailing quotes
-            command = re.sub(f'^(\"|\')(.*)(\"|\')$', r'\2', command)
-            expected = re.sub(f'^(\"|\')(.*)(\"|\')$', r'\2', expected)
+            field = re.sub(f'^(\"|\')(.*)(\"|\')$', r'\2', field)
 
-            self._tests_list[i] = (command, expected)
+            result.append(field)
 
-        debug.trace(7, f'pybats - sanitized tests: {self._tests_list}')
-
-
-    def get_tests(self, text):
-        """Extract and process tests from text"""
-        self._extract_from_text(text)
-        self._remove_indent()
-        self._sanitize()
-
-        debug.trace(7, f'pybats - final test list: {self._tests_list}')
-        return self._tests_list
+        debug.trace(7, f'batspp (test {self._debug_test_number}) - _sanitize({test}) => {result}')
+        return result
 
 
-# NOTE: this is not necessary but it improves readability.
-class CommandTestsExtractor(TestsExtractor):
-    """Class CommandTestsExtractor to extract and process specific command tests"""
+    def _last_process(self, test):
+        """Process test fields before convert into bats"""
+
+        # NOTE: if this is overrided, the result must be:
+        # result = ['indent', 'setup', 'actual', 'expected']
+
+        result = test
+
+        debug.trace(7, f'batspp (test {self._debug_test_number}) - _last_process({test}) => {result}')
+        return result
 
 
-class FunctionTestsExtractor(TestsExtractor):
-    """Class FunctionTestsExtractor to extract and process specific function tests"""
+    def _convert_to_bats(self, test):
+        """Convert tests to bats format"""
+        _, setup, actual, expected = test
+
+        result = ''
+
+        # Add test header
+        result += f'@test {repr(actual)[:-1]} should {"" if self._assert_equals else "NOT "}return {repr(expected)[1:]} {{\n'
+
+        # Add test setup
+        for setup_command in setup.splitlines():
+            if setup_command:
+                setup_command = re.sub(r'(^\$\s+|\n+$)', '', setup_command)
+                result       += f'\t{setup_command}\n'
+
+        # Add actual and expected (or not)
+        expected_var_name = f'{"" if self._assert_equals else "not_"}expected'
+
+        result += f'\tactual=$({actual})\n'
+        result += f'\t{expected_var_name}=${repr(expected)}\n'
+
+        # Add assertion
+        result += f'\t[ "$actual" {"==" if self._assert_equals else "!="} "${expected_var_name}" ]\n'
+
+        # End test
+        result += '}\n\n'
+
+        debug.trace(7, f'batspp (test {self._debug_test_number}) - _convert_to_bats({test}) =>\n{result}')
+        return result
 
 
-    def _disable_alias(self):
-        """Disable alias adding // in functions tests"""
-        for i, test in enumerate(self._tests_list):
-            command, expected = test
-            self._tests_list[i] = ('\\' + command, expected)
-        debug.trace(7, f'pybats - disabled alias to tests: {self._tests_list}')
+    def get_bats_tests(self, text):
+        """Returns BATS tests"""
+
+        debug.trace(7, f'batspp - pattern used ({self._re_flags}): {self._pattern}')
+
+        bats_tests = ''
+
+        for match in re.findall(self._pattern, text, flags=self._re_flags):
+
+            test = self._first_process(match)
+            if len(test) == 4:
+                test = self._sanitize(test)
+                test = self._last_process(test)
+                bats_tests += self._convert_to_bats(test)
+            else:
+                debug.trace(7, f'batspp (test {self._debug_test_number}) - wrong number of fields ({test}).')
+
+            self._debug_test_number += 1
+
+        return bats_tests
 
 
-    def get_tests(self, text):
-        """Extract and process tests from text"""
-        self._extract_from_text(text)
-        self._remove_indent()
-        self._disable_alias()
-        self._sanitize()
+class CommandTests(CustomTestsToBats):
+    """Extract and process specific command tests"""
 
-        debug.trace(7, f'pybats - final test list: {self._tests_list}')
-        return self._tests_list
+
+    def __init__(self):
+        flags = re.DOTALL | re.MULTILINE
+
+        pattern = (fr'(({INDENT_PATTERN}\$\s+[^\n]+\n)*)' # setup
+                   fr'({INDENT_PATTERN})\$\s+([^\n]+)\n'  # command test
+                   fr'(.+?)\n'                            # expected output
+                   fr'{INDENT_PATTERN}$')                 # end test
+
+        super().__init__(pattern, re_flags=flags)
+
+
+    def _first_process(self, match):
+        """First process after match, format matched results into [indent, setup, actual, expected]"""
+
+        #         indent     setup     actual   expected
+        result = [match[2], match[0], match[3], match[4]]
+
+        debug.trace(7, f'batspp (test {self._debug_test_number}) - _first_process({match}) => {result}')
+        return result
+
+
+class FunctionTests(CustomTestsToBats):
+    """Extract and process specific function tests"""
+
+
+    # Class globals
+    assert_eq     = r'=>'
+    assert_ne     = r'=\/>'
+
+
+    def __init__(self):
+
+        flags = re.MULTILINE
+
+        pattern = (fr'({INDENT_PATTERN})'                        # indent
+                   r'(.+?)'                                      # functions + args
+                   fr'\s+({self.assert_eq}|{self.assert_ne})\s+' # assertion
+                   r'((.|\n)+?)'                                 # expected output
+                   fr'\n{INDENT_PATTERN}$')                      # blank indent (end test)
+
+        super().__init__(pattern, re_flags=flags)
+
+
+    def _first_process(self, match):
+        """First process after match, format matched results into [indent, setup, actual, expected]"""
+
+        indent, actual, assertion, expected, _ = match
+
+        # Check assertion
+        if assertion == self.assert_eq:
+            self._assert_equals = True
+        elif assertion == self.assert_ne:
+            self._assert_equals = False
+        else:
+            self._assert_equals = None
+
+        # Format values
+        result = [indent, '', actual, expected]
+
+        debug.trace(7, f'batspp (test {self._debug_test_number}) - _first_process({match}) => {result}')
+        return result
+
+
+    def _last_process(self, test):
+        """Process test fields before convert into bats"""
+
+        indent, setup, actual, expected = test
+
+        # Disable alias adding '//' to functions
+        actual = '\\' + actual
+
+        result = [indent, setup, actual, expected]
+        debug.trace(7, f'batspp (test {self._debug_test_number}) - _last_process({test}) => {result}')
+        return result
 
 
 if __name__ == '__main__':
