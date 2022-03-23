c8550419c23995c8e8220dacd25de3f79f9d5788
Tom O'Hara <tomasohara@gmail.com>
2022-03-23 00:09:26 -0500

    initial version

---
+++ tomohara-settings.bash
@@ -0,0 +1,9 @@
+# idiosyncratic settings for user tomohara
+#
+# note:
+# - New place for settings previously put in tomohara-settings.bash.
+# TODO:
+# - flesh out
+#
+
+append-path "$TOM_BIN/bruno"
