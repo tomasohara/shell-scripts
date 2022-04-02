0272e61cab3aea90a29aef9d85d85156fd07eb37
Tom O'Hara <tomasohara@gmail.com>
2022-04-01 23:05:45 -0500

    adss mezcla init

--- tomohara-settings.bash
+++ tomohara-settings.bash
@@ -1,9 +1,11 @@
 # idiosyncratic settings for user tomohara
 #
 # note:
-# - New place for settings previously put in tomohara-settings.bash.
+# - New place for settings previously put in tomohara-aliases.bash.
 # TODO:
 # - flesh out
 #
 
+## OLD: prepend-path "$HOME/python/Mezcla/mezcla"
+add-python-path $HOME/python/Mezcla/mezcla
 append-path "$TOM_BIN/bruno"
