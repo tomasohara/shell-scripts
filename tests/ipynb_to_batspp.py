8bfe22d1583f5e4fa3921e02989fbe14aa3ce154
aveeyan <avyn.xyz@gmail.com>
2022-08-03 22:27:23 +0545

    Completed ipynb_to_batspp.py || Searching reasons for zero execution is batspp

--- tests/ipynb_to_batspp.py
+++ tests/ipynb_to_batspp.py
@@ -4,6 +4,18 @@ import sys
 import os
 
 filename_arg = sys.argv
-file2convert = filename_arg[1]
+file_in_ipynb = filename_arg[1]
 
-RENAME_COMMAND = f"mv {file2convert} {file2convert}.batspp"
+def batspp_rename():
+    file_in_batspp = file_in_ipynb.replace(".ipynb", ".batspp")#<--CONVERTS EXTENSION TO BATSPP
+    RENAME_COMMAND = f"mv {file_in_ipynb} {file_in_batspp}"#<--COMMAND TO BE EXECUTED
+    
+    os.system(RENAME_COMMAND)
+    print(f"Changes were made to {file_in_ipynb}.\n\nFILE RENAMED:\n{file_in_ipynb} --> {file_in_batspp}")
+
+# START OF MAIN #
+if ".ipynb" in file_in_ipynb:
+    batspp_rename();
+else:
+    print (f"The selected file is not an IPython file.\nNo changes were made to the file: {file_in_ipynb}")    
+# END OF MAIN #
