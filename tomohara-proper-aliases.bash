2c12c15552d4bdef8027ccb1d0a4369e77c521b7
Tom O'Hara <tomasohara@gmail.com>
2022-08-12 22:48:50 -0500

    initial version

---
+++ tomohara-proper-aliases.bash
@@ -0,0 +1,17 @@
+#! /usr/bin/env bash
+#
+# tomohara-proper-aliases.bash: aliases not intended for general consumption
+#
+
+# Change git-xyz-plus to git-xyz- for sake of tab completion
+# TODO: automate following
+alias git-add-='git-add-plus'
+alias git-diff-='git-diff-plus'
+alias git-difftool-='git-difftool-plus'
+alias git-log-='git-log-plus'
+alias git-update-='git-update-plus' 
+alias git-vdiff='git-vdiff-alias '
+## TODO: alias git-X-='git-X-plus'
+
+# Other misc. stuff
+alias nvidia-batched='batch-nvidia-smi.sh'
