b6ff23a95a158cf3162d51c76471ab3ece7ef0b8
Tom O'Hara <tohara@pandologic.com>
2022-12-31 20:03:06 -0600

    shell-check- filters out which warning

--- tomohara-aliases.bash
+++ tomohara-aliases.bash
@@ -2995,8 +2995,9 @@ function shell-check {
     # - SC2164: Use 'cd ... || exit' or 'cd ... || return' in case cd fails.
     # - SC2181 (style): Check exit code directly with e.g. 'if mycmd;' ...
     # - SC2219 (style): Instead of 'let expr', prefer (( expr )) .
+    # - SC2230: which is non-standard
     # - TODO2: -e 'SC1090,SC1091,SC2009,SC2012,SC2129,SC2164,SC2181'
-    shell-check-full "$@" | perl -0777 -pe 's/\n\s+(Did you mean:)/\n$1/g;' | perl-grep -para -v '(SC1090|SC1091|SC2009|SC2012|SC2129|SC2164|SC2181|SC2219)';
+    shell-check-full "$@" | perl -0777 -pe 's/\n\s+(Did you mean:)/\n$1/g;' | perl-grep -para -v '(SC1090|SC1091|SC2009|SC2012|SC2129|SC2164|SC2181|SC2219|SC2230)';
 }
 
 #-------------------------------------------------------------------------------
