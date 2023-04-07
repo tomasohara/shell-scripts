44f2b933b128f727552e683ef1faa42da4ba3e19
Tom O'Hara <tohara@tohara.com>
2023-04-07 13:34:22 -0500

    fix to put sourcing after arg check and tracing likewise delayed; shows error in list of files to merge

--- adhoc/consolidate-notes.bash
+++ adhoc/consolidate-notes.bash
@@ -12,28 +12,17 @@
 #       # format: m h dom mon dow command
 #
 
-## DEBUG: echo "0. $@"
-
-# Enable Bash aliases, etc.
-## maldito shellcheck: [SC1090: Can't follow non-constant source]
-# shellcheck disable=SC1090
-{
-    shopt -s expand_aliases
-    ## DEBUG: echo "1. $@"
-    source ~/bin/tomohara-aliases.bash
-    ## DEBUG: echo "2. $@"
-    source ~/bin/tomohara-proper-aliases.bash
-    ## DEBUG: echo "3. $@"
-    source ~/bin/tomohara-settings.bash
-}
+## DEBUG:
+echo "0. $@"
 
 # Uncomment following line(s) for tracing:
 # - xtrace shows arg expansion (and often is sufficient)
 # - verbose shows source commands as is (but usually is superfluous w/ xtrace)
 #  
+## DEBUG:
 ## echo "$@"
 ## set -o xtrace
-## DEBUG: set -o verbose
+## DETAILED DEBUG: set -o verbose
 
 # Show usage statement
 # TODO: convert into a function that get invoked when $1 is empty or --help
@@ -66,10 +55,11 @@ fi
 # TODO: set getopt-type utility
 #
 moreoptions=0; case "$1" in -*) moreoptions=1 ;; esac
+trace=0
 while [ "$moreoptions" = "1" ]; do
     # TODO : add real options
     if [ "$1" = "--trace" ]; then
-	set -o xtrace
+	trace=1
     elif [ "$1" = "--fubar" ]; then
 	echo "fubar"
     elif [[ ("$1" = "--") || ("$1" = "-") ]]; then
@@ -81,6 +71,25 @@ while [ "$moreoptions" = "1" ]; do
     shift;
     moreoptions=0; case "$1" in -*) moreoptions=1 ;; esac
 done
+
+# Enable Bash aliases, etc.
+## maldito shellcheck: [SC1090: Can't follow non-constant source]
+# shellcheck disable=SC1090
+{
+    shopt -s expand_aliases
+    ## DEBUG: echo "1. $@"
+    source ~/bin/tomohara-aliases.bash
+    ## DEBUG: echo "2. $@"
+    source ~/bin/tomohara-proper-aliases.bash
+    ## DEBUG: echo "3. $@"
+    source ~/bin/tomohara-settings.bash
+}
+
+# Enable tracing
+# note: done after alias files sourced to avoid exxtraenous tracing
+if [ "$trace" = "1" ]; then
+    set -o xtrace
+fi
 
 # Get settings from environment
 # TODO: expand env. options
@@ -102,11 +111,19 @@ set-xterm-title "merge-notes [$PWD]"
 # note: uses 'wider net' for adhoc notes; uses *notes* to avoid incorporating existing consolidationms (e.g., _master-note-info.list)
 # TODO: -o -iname '*.txt' 
 # TODO: make '*.txt' optional; check effect on typo in merge_notes arg parsing
+# TODO2: use two-part filtering (e.g., first find and then grep)
+# TODO1: filter inaccesible files, such as emacs lock files
+#     .#_toms-mac-adhoc-notes-jan23.txt -> tohara@tohara-mac.lan.50018
 if [ "${ALL_TEXT:-0}" = "1" ]; then
     # TODO: rework so that pattern-type options specified individially (e.g., LOG_FILES, ADHOC_NOTES, etc)
-    find "${SRC_DIR:-.}" \(  -iname '*.txt' -o -iname '*.text' \) 2> "$new_base.files.log" | egrep -iv '/(backup|temp)/' | perl -pe 's/ /\\ /g;' > "$new_base.files.list";
+    find "${SRC_DIR:-.}" \(  -iname '*.txt' -o -iname '*.text' \) 2> "$new_base.files.log" | egrep -iv '/(backup|old|temp)/' | perl -pe 's/ /\\ /g;' > "$new_base.files.list";
 else
-    find "${SRC_DIR:-.}" \( -iname '*adhoc*[0-9][0-9]*.txt*' -o -iname '*adhoc*[0-9][0-9]*.log*' -o -iname '*-notes*.txt' -o -iname '*-notes*.list' -o -iname '*-notes*.log' \) 2> "$new_base.files.log" | egrep -iv '/(backup|temp)/' | perl -pe 's/ /\\ /g;' > "$new_base.files.list";
+    find "${SRC_DIR:-.}" \( -iname '*adhoc*[0-9][0-9]*.txt*' -o -iname '*adhoc*[0-9][0-9]*.log*' -o -iname '*-notes*.txt' -o -iname '*-notes*.list' -o -iname '*-notes*.log' \) 2> "$new_base.files.log" | egrep -iv '/(backup|old|temp)/' | perl -pe 's/ /\\ /g;' > "$new_base.files.list";
+fi
+## TODO: if [ -z "$new_base.files.list" ]; then
+if [ "$(wc -l < "$new_base.files.list")" = "0" ]; then
+    echo "Error: problem finding note files to merge" 1>&2
+    exit
 fi
 check-errors-excerpt "$new_base.files.log"
 
@@ -123,6 +140,8 @@ chmod ugo-w $new_base*;
 # Rename existing master note files
 # TODO: exclude cases already with timestamps
 ## OLD: rename-with-file-date "$base.files.list" "$base.list" "$base.log" "$base.files.log"
+# TODO1: fix following
+#    "_master-note-info.list.06apr23.log" already exists! not mv'ing "_new-_master-note-info.list.06apr23.log" "_master-note-info.list.06apr23.log"
 rename-with-file-date "$base.files.list" "$base.list" "$base.log" "$base.files.log" 2>&1 | egrep -v "\d{2}\w+\d{2}.*already exists"
 
 # Rename new file to use target name
