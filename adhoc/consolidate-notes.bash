15ad4e798ca888751dd7eabfffa17e2d7aacf2cd
Tom O'Hara <tohara@tohara.com>
2023-04-10 11:24:03 -0500

    uses absoulte path; filters misc log files; fixes renaming steps

--- adhoc/consolidate-notes.bash
+++ adhoc/consolidate-notes.bash
@@ -97,18 +97,12 @@ base="_master-note-info";
 new_base="_new-$base"
 ## TODO: cd ~/notes-archive
 cd "${TARGET_DIR:-.}"
+## TEMP: ensure full path used (TODO: only if SRC_DIR equals TARGET_DIR)
+cd-realdir
 set-xterm-title "merge-notes [$PWD]"
 
-## OLD:
-## init-conda-arm64
-## conda-activate-env nlp-py-3-9
-## ## alt: support for maldito mac-m1
-## ## init-conda-x86_64; conda activate test-py-3-6-x86_64
-## ## TEMP
-## prepend-path /opt/homebrew/Caskroom/miniforge/base/envs/nlp-py-3-9/bin
-
 # Prepare list of notes files, excluding backup and temp directories (i.e., files with paths like */backup/* or */temp/*)
-# note: uses 'wider net' for adhoc notes; uses *notes* to avoid incorporating existing consolidationms (e.g., _master-note-info.list)
+# note: uses 'wider net' for adhoc notes: requires 'notes' not 'note' to avoid incorporating existing consolidationms (e.g., _master-note-info.list)
 # TODO: -o -iname '*.txt' 
 # TODO: make '*.txt' optional; check effect on typo in merge_notes arg parsing
 # TODO2: use two-part filtering (e.g., first find and then grep)
@@ -118,7 +112,9 @@ if [ "${ALL_TEXT:-0}" = "1" ]; then
     # TODO: rework so that pattern-type options specified individially (e.g., LOG_FILES, ADHOC_NOTES, etc)
     find "${SRC_DIR:-.}" \(  -iname '*.txt' -o -iname '*.text' \) 2> "$new_base.files.log" | egrep -iv '/(backup|old|temp)/' | perl -pe 's/ /\\ /g;' > "$new_base.files.list";
 else
-    find "${SRC_DIR:-.}" \( -iname '*adhoc*[0-9][0-9]*.txt*' -o -iname '*adhoc*[0-9][0-9]*.log*' -o -iname '*-notes*.txt' -o -iname '*-notes*.list' -o -iname '*-notes*.log' \) 2> "$new_base.files.log" | egrep -iv '/(backup|old|temp)/' | perl -pe 's/ /\\ /g;' > "$new_base.files.list";
+    # Note: Filters log files starting with _ unless adhoc in the name (e.g., /home/tohara/config/_consolidate-experiment-notes-05apr23.log).
+    # This uses the perl-style zero-width negative lookahead regex.
+    find "${SRC_DIR:-.}" \( -iname '*adhoc*[0-9][0-9]*.txt*' -o -iname '*adhoc*[0-9][0-9]*.log*' -o -iname '*-notes*.txt' -o -iname '*-notes*.list' -o -iname '*-notes*.log' \) 2> "$new_base.files.log" | egrep -iv '/(backup|old|temp)/' | egrep -iv '/_[^\/]+(?!adhoc)[^\/]+.log' | perl -pe 's/ /\\ /g;' > "$new_base.files.list";
 fi
 ## TODO: if [ -z "$new_base.files.list" ]; then
 if [ "$(wc -l < "$new_base.files.list")" = "0" ]; then
@@ -131,18 +127,17 @@ check-errors-excerpt "$new_base.files.log"
 # TODO: check stdin support in main.py
 # maldito shellcheck: SC2086 (info): Double quote to prevent globbing and word splitting.
 # shellcheck disable=SC2086
-DEBUG_LEVEL=4 xargs $PYTHON -m mezcla.merge_notes --ignore-dividers --output-dividers --show-file-info < "$new_base.files.list" > "$new_base.list" 2> "$new_base.list.$(TODAY).log";
-check-errors-excerpt "$new_base.list.$(TODAY).log";
+DEBUG_LEVEL=4 xargs $PYTHON -m mezcla.merge_notes --ignore-dividers --output-dividers --show-file-info < "$new_base.files.list" > "$new_base.list" 2> "$new_base.list.log";
+check-errors-excerpt "$new_base.list.log";
 
 # Set read-only
 chmod ugo-w $new_base*;
 
 # Rename existing master note files
 # TODO: exclude cases already with timestamps
-## OLD: rename-with-file-date "$base.files.list" "$base.list" "$base.log" "$base.files.log"
-# TODO1: fix following
-#    "_master-note-info.list.06apr23.log" already exists! not mv'ing "_new-_master-note-info.list.06apr23.log" "_master-note-info.list.06apr23.log"
-rename-with-file-date "$base.files.list" "$base.list" "$base.log" "$base.files.log" 2>&1 | egrep -v "\d{2}\w+\d{2}.*already exists"
+rename-with-file-date "$base.files.list" "$base.list" "$base.log" "$base.files.log"
 
 # Rename new file to use target name
-rename-files "$new_base" "$base" "$new_base"*
+# TODO1: fix following
+#    "_master-note-info.list.06apr23.log" already exists! not mv'ing "_new-_master-note-info.list.06apr23.log" "_master-note-info.list.06apr23.log"
+rename-files "$new_base" "$base" $(ls "$new_base"* | egrep -v "\d{2}\w+\d{2}")
