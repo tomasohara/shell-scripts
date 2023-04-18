9115b566b31030963b1c2cabfc98fd6d673c148a
Tom O'Hara <tohara@tohara.com>
2023-04-18 18:00:46 -0500

    fixes test_arithmetic_expression, and OPENAI_API_KEY spec; revises tests to account for overhaul to main script; add more xfail tests; adds detailed tracing; drops python3 since 2 is dead; normalized whitespace in actual vs expected; omits comments in generated code; script invokes pytest automatically; adds prioritized todo notes; tips on disabling debug tracing; code cleanup

--- tests/test_bash2python.py
+++ tests/test_bash2python.py
@@ -1,5 +1,3 @@
-#!/usr/bin/env pytest
-## TODO (have the script invoke pytest: see mezcla/tests/test_system.py)
 #!/usr/bin/env python
 #
 # pylint: disable=line-too-long
@@ -10,39 +8,84 @@
 #
 # Notes:
 # - Use 'pytest test_bash2python.py -k "not skip"' to skip the tests that cover functions not yet implemented.
+# - For OpenAI key specification, see
+#      https://platform.openai.com/docs/api-reference/introduction?lang=python
+# - Debug tracing might cause problems for the tests, so disable as follows:
+#      DEBUG_LEVEL=0 pytest tests/test_bash2python.py
+#   Alternatively, use PYTHONOPTIMIZE=2 so that stubs used all debug.xyz functions.
+#
+# Tips:
+# - ** See bash2python.py for important coding tips.
+#
+# TODO2:
+# - * Err on the side of adding more tests!
+#
+
 """Tests for bash2python.py"""
 
+# Standard modules
 import os
 import subprocess
+
+# Installed modules
 import pytest
 from click.testing import CliRunner
+
+# Local modules
 from bash2python import Bash2Python as bp
 from bash2python_diff import main
+from mezcla import debug
+from mezcla.my_regex import my_re
 
-def run_bash(bash, python):
+#-------------------------------------------------------------------------------
+# Helper functions
+
+def run_bash(bash, python):                # Note: run_bash no longer used
     """Helper for call to bash2python"""
     output = (
-        subprocess.check_output(["python3", "bash2python.py", "--script", bash])
+        subprocess.check_output(["python", "bash2python.py", "--script", bash])
         .decode()
         .strip()
     )
     assert output == python
 
 
-def bash2py(bash, python):
-    """Helper for call bash2python as a module"""
-    b2p = bp(bash, None)
+def normalize_whitespace(text):
+    """Perform normalization of TEXT to facilitate comparisons: carriage returns changed to newlines, spaces and newlines are collapsed, and outer whitespace is stripped"""
+    # EX: normalize("Hey  Joe\r\n!") => Hey Joe\n!"                                    
+    result = text.strip()
+    result = my_re.sub(r"\r", "\n", result)
+    result = my_re.sub(r"\s+\n+\s+", "\n", result)
+    result = my_re.sub(r"  +", " ", result)
+    debug.trace(7, f"normalize({text!r}) => {result!r}")
+    return result
+
+
+def bash2py(bash, python, skip_normalize=False, keep_comments=False):
+    """Helper for call bash2python as a module
+    Notes:
+    - Applies whitespace normalization unless SKIP_NORMALIZE
+    - Omits conversion comments unless KEEP_COMMENTS (for sake of simpler matching)
+    """
+    b2p = bp(bash, None, skip_comments=(not keep_comments))
     output = b2p.format(False)
+    if not skip_normalize:
+        output = normalize_whitespace(output)
+        python = normalize_whitespace(python)
+    debug.trace_expr(5, output, python, delim="\n")
     assert output == python
 
-@pytest.mark.skipif(not os.getenv('API_KEY'), reason='API_KEY not set')
+#-------------------------------------------------------------------------------
+# Tests proper
+    
+@pytest.mark.skipif(not os.getenv('OPENAI_API_KEY'), reason='OPENAI_API_KEY not set')
 def test_codex():
     """Simple test for codex using an very standard input"""
     bash = "echo Hello World"
     python = bp.codex_convert(None, bash)
     output = 'print("Hello World")'
     if python == '# internal error':
-        print("Usually a problem with API_KEY")
+        print("Usually a problem with OPENAI_API_KEY")
     assert output in python
 
 
@@ -55,8 +98,12 @@ def test_variable_substitution():
 
 def test_operator_substitution():
     """Test if simple operators are substituted in a if statement"""
-    bash = "if [ 1 -gt 0 -lt 2]; then echo Hello; fi"
-    python = """#b2py: Founded loop of order 1. Please be careful\nif  1 > 0 < 2:\n    run('echo Hello')\n"""
+    ## OLD: bash = "if [ 1 -gt 0 -lt 2 ]; then echo Hello; fi"
+    ## NOTE: above made into xfail one below
+    bash = 'if [ 1 -gt 0 -lt 2 ]; then\n   echo "Hello"\nfi'
+    # TODO2: test for warning separately (e.g., strip comments); also, use valid bash input
+    ## OLD: python = """#b2py: Founded loop of order 1. Please be careful\nif  1 > 0 < 2:\n    run('echo Hello')\n"""
+    python = """if 1 > 0 < 2:\n    run('echo "Hello"')\n"""
     bash2py(bash, python)
 
 
@@ -71,15 +118,24 @@ def test_comments_2():
     """Checks in-line comments to ensure integrity"""
     bash = "echo $foo # Another comment"
     python = "run(f'echo {foo} ') #  Another comment"
-    bash2py(bash, python)
+    bash2py(bash, python, keep_comments=True)
 
 
 def test_while_loop():
     """Test a simple while loop convertion"""
-    bash = """while [ $i -le 5 ]; do
-    echo $i
-    done"""
-    python = "#b2py: Founded loop of order 1. Please be careful\nwhile  {i} <= 5 :\n    run(f'echo {i}')\n"
+    debug.trace(4, "test_while_loop()")
+    bash = """
+c=3
+while [ $c -le 5 ]; do
+    echo $c
+done
+"""
+    ## OLD: python = "#b2py: Founded loop of order 1. Please be careful\nwhile i <= 5 :\n    run(f'echo {i}')\n"
+    python = """
+c = 3
+while c <= 5:
+    run(f'echo {c}')
+"""
     bash2py(bash, python)
 
 
@@ -99,15 +155,18 @@ def test_command_pipe():
 
 def test_variable_assignment():
     """Tests a simple case of variable assignment in two lines"""
-    bash = "name='John Doe'; echo $name"
-    python = "name = 'John Doe'\n run(f'echo {name}')"
+    ## OLD: bash = "name='John Doe'; echo $name"
+    ## NOTE: need newline so echo resolved properly (old test moved below as xfail)
+    bash = "name='John Doe'\necho $name"
+    python = "name = 'John Doe'\nrun(f'echo {name}')"
     bash2py(bash, python)
 
 
 def test_arithmetic_expression():
     """Checks if arithmetics are working correctly"""
     bash = "echo $((2+3*4))"
-    python = "run('echo $((2+3*4))', skip_print=False)"
+    ## OLD: python = "run('echo $((2+3*4))', skip_print=False)"
+    python = "run('echo f\"{2+3*4}\"')"
     bash2py(bash, python)
 
 
@@ -120,24 +179,27 @@ def test_double_quotes():
 
 def test_embedded_for_not_supported():
     """Make sure that embedded FOR issues warning about not being supported"""
-    bash = """for i in 1 2 3; do
-    for j in a b c; do
-    echo \"$i$j\"
-    done
+    bash = """
+    for i in 1 2 3; do
+        for j in a b c; do
+            echo \"$i vs. $j\"
+        done
     done 
     """
-    python = "embedded for: for i in 1 2 3; do"
+    ## OLD: python = "embedded for: for i in 1 2 3; do"
+    python = "embedded for"
     output = (
-        subprocess.check_output(["python3", "bash2python.py", "--script", bash])
+        subprocess.check_output(["python", "bash2python.py", "--script", bash])
         .decode()
         .strip()
     )
+    debug.trace_expr(4, python)
     assert python in output
 
 
 @pytest.mark.skip
 def test_if_else_condition():
-    """TODO flesh out: test if/else"""
+    """TODO3 flesh out comment: test if/else"""
     bash = """if [ -f "$1" ]; then'
            echo "$1 exists and is a regular file."
            elif [ -e "$1" ]; then
@@ -233,26 +295,60 @@ def test_complex_for_loop():
     ("exit", "run('exit')"),
     ("foo=$bar", "foo = f'{bar}'"),
     ("ls -a", "run('ls -a')"),
-    ("echo $bar", "run(f'echo {bar}')")
+    ("echo $bar", "run(f'echo {bar}')"),
+    # arithmetic expressions and expansions (latter results in string)
+    ("(( z = x + y ))", "z = x + y"),
+    ("z=$((x + y))", 'z = f"{x + y}"'),
+    # simple if statement(s)
+    pytest.param("""if [ $? -eq 0 ]; then echo "Success"; fi""",
+                 """if run("echo $?") == "0":\n   print("Success")""", marks=pytest.mark.xfail),
+    pytest.param("""if [ 1 -gt 0 -lt 2 ]; then echo Hello; fi""",    # note: single line causes problem
+                 """if 1 > 0 < 2:\n    run('echo Hello')\n""", marks=pytest.mark.xfail),
+    # simple for statement(s)
+    pytest.param("""for i in 1 2 3; do echo "$i"; done""",
+                 """for i in [1, 2, 3]: print(i)""", marks=pytest.mark.xfail),
+    # simple while statement(s)
+    pytest.param("""while [ $i -ge 0 ]; do""",
+                 """while i >= 0:\n    print(i)""", marks=pytest.mark.xfail),
+    # variable assignment
+    pytest.param("""name='John Doe'; echo $name""",                  # semicolon blocks variable conversion
+                 """name = 'John Doe'\n run(f'echo {name}')""", marks=pytest.mark.xfail),
+    # no-op statement
+    ("true", "pass"),
+    pytest.param("true;", "pass", marks=pytest.mark.xfail),          # semicolon needs to be dropped
     ])
 def test_tabular_tests(bash, python):
     """Tests in tabular format. Uses pythest parametrize"""
     bash2py(bash, python)
 
-
+## TODO1: put bash2python_diff tests in test_bash2python_diff
+##
+@pytest.mark.skipif(not os.getenv('OPENAI_API_KEY'), reason='OPENAI_API_KEY not set')
 def test_diff_no_opts():
     """Tests bash2python_diff without flags enabled"""
+    debug.trace(5, "test_diff_no_opts()")
     runner = CliRunner()
     result = runner.invoke(main, input="line1\n\nline2\n")
+    debug.trace_expr(5, result)
+    debug.trace_object(6, result)
     assert result.exit_code == 0
     assert "------------codex------------" in result.output
     assert "------------b2py------------" in result.output
-
-@pytest.mark.skipif(not os.getenv('API_KEY'), reason='API_KEY not set')
+##
+@pytest.mark.skipif(not os.getenv('OPENAI_API_KEY'), reason='OPENAI_API_KEY not set')
 def test_diff_opts():
     """Tests bash2python_diff with all flags enabled"""
+    debug.trace(5, "test_diff_opts()")
     runner = CliRunner()
     result = runner.invoke(main, ["--perl", "--diff"], input="echo Hello World\n\nfoo=bar\n")
+    debug.trace_expr(5, result)
+    debug.trace_object(6, result)
     assert result.exit_code == 0
     assert 'print("Hello World")' in result.output
     assert "foo = bar" in result.output
+
+#-------------------------------------------------------------------------------
+
+if __name__ == '__main__':
+    debug.trace_current_context()
+    pytest.main([__file__])
