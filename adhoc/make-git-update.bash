4c28c3eeb3fc545f22c39c536be1a89f2d09d099
Tom O'Hara <tohara@tohara.com>
2023-05-08 19:11:10 -0500

    initial version: Wrapper aroung git-template alias for script-update usage

---
+++ adhoc/make-git-update.bash
@@ -0,0 +1,18 @@
+#! /usr/bin/env bash
+#
+# Wrapper aroung git-template alias and also invokes a bash shell
+# note: used in script-udpdate alias to show template in new shell for git updates
+
+# Enable Bash aliases, etc.
+## maldito shellcheck: [SC1090: Can't follow non-constant source]
+# shellcheck disable=SC1090
+{
+    shopt -s expand_aliases
+    source ~/bin/tomohara-aliases.bash
+    source ~/bin/tomohara-proper-aliases.bash
+    source ~/bin/tomohara-settings.bash
+}
+
+# Show template and then start a bash session
+git-template
+bash -i
