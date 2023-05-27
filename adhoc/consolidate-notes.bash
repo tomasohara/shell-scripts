35758cca55f1928ce26a7ff43b21379e2e1fb577
Tom O'Hara <tohara@tohara.com>
2023-05-26 21:11:39 -0500

    add adhoc .list files; documents patterns clearer; shell check filter

--- adhoc/consolidate-notes.bash
+++ adhoc/consolidate-notes.bash
@@ -102,6 +102,12 @@ cd-realdir
 set-xterm-title "merge-notes [$PWD]"
 
 # Prepare list of notes files, excluding backup and temp directories (i.e., files with paths like */backup/* or */temp/*)
+# Inclusion pattern:
+# - *notes*{txt,list,log}
+# - *adhoc*[0-9][0-9]*{txt,list,log}
+# Exclusion pattern:
+# - backup|old|temp
+# - _*adhoc*.log
 # note: uses 'wider net' for adhoc notes: requires 'notes' not 'note' to avoid incorporating existing consolidationms (e.g., _master-note-info.list)
 # TODO: -o -iname '*.txt' 
 # TODO: make '*.txt' optional; check effect on typo in merge_notes arg parsing
@@ -114,7 +120,8 @@ if [ "${ALL_TEXT:-0}" = "1" ]; then
 else
     # Note: Filters log files starting with _ unless adhoc in the name (e.g., /home/tohara/config/_consolidate-experiment-notes-05apr23.log).
     # This uses the perl-style zero-width negative lookahead regex.
-    find "${SRC_DIR:-.}" \( -iname '*adhoc*[0-9][0-9]*.txt*' -o -iname '*adhoc*[0-9][0-9]*.log*' -o -iname '*-notes*.txt' -o -iname '*-notes*.list' -o -iname '*-notes*.log' \) 2> "$new_base.files.log" | egrep -iv '/(backup|old|temp)/' | egrep -iv '/_[^\/]+(?!adhoc)[^\/]+.log' | perl -pe 's/ /\\ /g;' > "$new_base.files.list";
+    ## OLD: find "${SRC_DIR:-.}" \( -iname '*adhoc*[0-9][0-9]*.txt*' -o -iname '*adhoc*[0-9][0-9]*.log*' -o -iname '*-notes*.txt' -o -iname '*-notes*.list' -o -iname '*-notes*.log' \) 2> "$new_base.files.log" | egrep -iv '/(backup|old|temp)/' | egrep -iv '/_[^\/]+(?!adhoc)[^\/]+.log' | perl -pe 's/ /\\ /g;' > "$new_base.files.list";
+    find "${SRC_DIR:-.}" \( -iname '*adhoc*[0-9][0-9]*.txt' -o -iname '*adhoc*[0-9][0-9]*.list' -o -iname '*adhoc*[0-9][0-9]*.log' -o -iname '*-notes*.txt' -o -iname '*-notes*.list' -o -iname '*-notes*.log' \) 2> "$new_base.files.log" | egrep -iv '/(backup|old|temp)/' | egrep -iv '/_[^\/]+(?!adhoc)[^\/]+.log' | perl -pe 's/ /\\ /g;' > "$new_base.files.list";
 fi
 ## TODO: if [ -z "$new_base.files.list" ]; then
 if [ "$(wc -l < "$new_base.files.list")" = "0" ]; then
@@ -143,4 +150,6 @@ rename-with-file-date "$base.files.list" "$base.files.log" "$base.list" "$base.l
 # Rename new file to use target name
 # TODO1: fix following
 #    "_master-note-info.list.06apr23.log" already exists! not mv'ing "_new-_master-note-info.list.06apr23.log" "_master-note-info.list.06apr23.log"
+# maldito shellcheck: SC2046: Quote this to prevent word splitting
+# shellcheck disable=SC2046
 rename-files "$new_base" "$base" $(ls "$new_base"* | egrep -v "\d{2}\w+\d{2}")
