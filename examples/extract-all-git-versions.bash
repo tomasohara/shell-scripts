7865535d446f9534abd09aa566c8f907bcc04c32
Tom O'Hara <tohara@tohara.com>
2023-06-19 17:31:32 -0500

    put data/version suffix after basename; cleanup, such as via shellcheck

--- examples/extract-all-git-versions.bash
+++ examples/extract-all-git-versions.bash
@@ -9,7 +9,7 @@
 EXPORT_TO=/tmp/all_versions_exported
 
 # take relative path to the file to inspect
-GIT_PATH_TO_FILE=$1
+GIT_PATH_TO_FILE="$1"
 
 # ---------------- don't edit below this line --------------
 
@@ -22,37 +22,50 @@ if [ "${GIT_PATH_TO_FILE}" == "" ]; then
 fi
 
 # check if file exist
-if [ ! -f ${GIT_PATH_TO_FILE} ]; then
+if [ ! -f "${GIT_PATH_TO_FILE}" ]; then
     echo "error: File '${GIT_PATH_TO_FILE}' does not exist. ${USAGE}" >&2
     exit 1
 fi
 
 # extract just a filename from given relative path (will be used in result file names)
-GIT_SHORT_FILENAME=$(basename $GIT_PATH_TO_FILE)
+GIT_SHORT_FILENAME=$(basename "$GIT_PATH_TO_FILE")
 
 # create folder to store all revisions of the file
-if [ ! -d ${EXPORT_TO} ]; then
+if [ ! -d "${EXPORT_TO}" ]; then
     echo "creating folder: ${EXPORT_TO}"
-    mkdir ${EXPORT_TO}
+    mkdir "${EXPORT_TO}"
 fi
 
 ## uncomment next line to clear export folder each time you run script
-#rm ${EXPORT_TO}/*
+#rm "${EXPORT_TO}"/*
 
 # reset coutner
 COUNT=0
 
 # iterate all revisions
-git rev-list --all --objects -- ${GIT_PATH_TO_FILE} | \
-    cut -d ' ' -f1 | \
-while read h; do \
-     COUNT=$((COUNT + 1)); \
-     COUNT_PRETTY=$(printf "%04d" $COUNT); \
-     COMMIT_DATE=`git show $h | head -3 | grep 'Date:' | awk '{print $4"-"$3"-"$6}'`; \
-     if [ "${COMMIT_DATE}" != "" ]; then \
-         git cat-file -p ${h}:${GIT_PATH_TO_FILE} > ${EXPORT_TO}/${COUNT_PRETTY}.${COMMIT_DATE}.${h}.${GIT_SHORT_FILENAME};\
-     fi;\
-done    
+##
+## OLD:
+## git rev-list --all --objects -- ${GIT_PATH_TO_FILE} | \
+##     cut -d ' ' -f1 | \
+## while read h; do \
+##      COUNT=$((COUNT + 1)); \
+##      COUNT_PRETTY=$(printf "%04d" $COUNT); \
+##      COMMIT_DATE=`git show $h | head -3 | grep 'Date:' | awk '{print $4"-"$3"-"$6}'`; \
+##      if [ "${COMMIT_DATE}" != "" ]; then \
+##          git cat-file -p ${h}:${GIT_PATH_TO_FILE} > ${EXPORT_TO}/${COUNT_PRETTY}.${COMMIT_DATE}.${h}.${GIT_SHORT_FILENAME};\
+##      fi;\
+## done    
+##
+# note: -r treats backslash literally
+(while read -r h; do 
+     COUNT=$((COUNT + 1));
+     COUNT_PRETTY=$(printf "%04d" $COUNT);
+     COMMIT_DATE=$(git show "$h" | head -3 | grep 'Date:' | awk '{print $4"-"$3"-"$6}');
+     if [ "${COMMIT_DATE}" != "" ]; then
+         git cat-file -p "${h}:${GIT_PATH_TO_FILE}" > "${EXPORT_TO}/${GIT_SHORT_FILENAME}.${COUNT_PRETTY}.${COMMIT_DATE}";
+ 
+     fi;
+done) <<<"$(git rev-list --all --objects -- "${GIT_PATH_TO_FILE}" |  cut -d ' ' -f1)"
 
 # return success code
 echo "result stored to ${EXPORT_TO}"
