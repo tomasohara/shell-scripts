908051b1d06e0d8c52a34578bbf2f8e66ed3f368
Tom O'Hara <tohara@tohara.com>
2023-07-06 11:40:39 -0500

    uses python3 due to docker quirk and changes output dir to /home/tomohara/temp or ~/temp

--- tests/summary_stats.bash
+++ tests/summary_stats.bash
@@ -13,7 +13,7 @@ cd $(dirname "$0")
 ## TODO by Aviyan: fix this to produce a simple summary as requested several
 ## times in email. (This was both to you and Bruno a while back and then more
 ## recently to you and Tana.)
-python3 ./batspp_report.py --txt
+OUTPUT_DIR=$TMP python3 ./batspp_report.py --txt
 
 ## NOTE: kcov is not critical and moreover is just serving to slow us down!
 ## TODO: python3 ./kcov_result.py --list --summary --export | tee summary_stats.txt
