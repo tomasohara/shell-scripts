58b758506068d4472dabb3157a999c56cf8a1308
Tom O'Hara <tohara@tohara.com>
2023-07-09 18:48:16 -0500

    adds BATSPP_OUTPUT w/ default of timestamped fix under ~/temp and likewise BATSPP_TEMP under /tmp; adds env. options for tracing; misc. cleanup

--- tests/summary_stats.bash
+++ tests/summary_stats.bash
@@ -1,18 +1,34 @@
 #!/bin/bash
-
+#
 # summary_stats.bash works in the following manner:
 # 1) ./batspp_report.py -k (regenerates all KCOV dirs and output in HTML)
 # 2) [optional] ./kcov_result.py --list --export (returns result according to the KCOV outputs) 
 # 3) Output of the process is also stored in ./summary_stats.txt
+#
 
+# Set bash regular and/or verbose tracing
+if [ "${TRACE:-0}" = "1" ]; then
+    set -o xtrace
+fi
+if [ "${VERBOSE:-0}" = "1" ]; then
+    set -o verbose
+fi
+
+# Change into testing script directory (e.g., ~/shell-scripts/tests)
 cd "$(dirname "$0")"
 
-TEMP="${TEMP:-$HOME/temp}"
-mkdir --parents "$TEMP"
+# Derive name for output file
+# Note: Uses unique temporary directory unless BATSPP_TEMP specified.
+# Note: ./temp subdirectry used because BatsPP generate many temp files
+timestamp=$(date '+%d%b%y-%H%M')
+TMP=${"TMP":-/tmp}
+BATSPP_OUTPUT="${BATSPP_TEMP:-"$HOME/temp/BatsPP-$timestamp"}"
+BATSPP_TEMP="${BATSPP_TEMP:-"$TMP/BatsPP-$timestamp"}"
+mkdir --parents "$BATSPP_OUTPUT" "$BATSPP_TEMP"
+echo "FYI: Using $BATSPP_OUTPUT for ouput and $BATSPP_TEMP for temp. files"
 
 # Run the set of alias tests, making sure Tom's aliases are defined
-# Note: 
-OUTPUT_DIR=$TEMP python3 ./batspp_report.py --txt --definitions ../all-tomohara-aliases-etc.bash
+OUTPUT_DIR="$BATSPP_OUTPUT" TEMP_BASE="$BATSPP_TEMP" python3 ./batspp_report.py --txt --definitions ../all-tomohara-aliases-etc.bash
 
 ## NOTE: kcov is not critical, so it is not run as part of workflow tests
 ## TODO: python3 ./kcov_result.py --list --summary --export | tee summary_stats.txt
