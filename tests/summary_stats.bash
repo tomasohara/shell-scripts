5dc8d994c49e7c52edaf31e8fd5c0c70e422b286
Tom O'Hara <tohara@tohara.com>
2023-07-09 16:28:46 -0500

    adds --definitions ../all-tomohara-aliases-etc.bash; cleanup comments

--- tests/summary_stats.bash
+++ tests/summary_stats.bash
@@ -2,21 +2,17 @@
 
 # summary_stats.bash works in the following manner:
 # 1) ./batspp_report.py -k (regenerates all KCOV dirs and output in HTML)
-# 2) ./kcov_result.py --list --export (returns result according to the KCOV outputs) 
+# 2) [optional] ./kcov_result.py --list --export (returns result according to the KCOV outputs) 
 # 3) Output of the process is also stored in ./summary_stats.txt
-# BUG: Might fail on first run
 
-## OLD: ./batspp_report.py -k && ./kcov_result.py --list --summary --export | tee summary_stats.txt
+cd "$(dirname "$0")"
 
-cd $(dirname "$0")
-
-## TODO by Aviyan: fix this to produce a simple summary as requested several
-## times in email. (This was both to you and Bruno a while back and then more
-## recently to you and Tana.)
 TEMP="${TEMP:-$HOME/temp}"
 mkdir --parents "$TEMP"
-OUTPUT_DIR=$TEMP python3 ./batspp_report.py --txt
 
-## NOTE: kcov is not critical and moreover is just serving to slow us down!
+# Run the set of alias tests, making sure Tom's aliases are defined
+# Note: 
+OUTPUT_DIR=$TEMP python3 ./batspp_report.py --txt --definitions ../all-tomohara-aliases-etc.bash
+
+## NOTE: kcov is not critical, so it is not run as part of workflow tests
 ## TODO: python3 ./kcov_result.py --list --summary --export | tee summary_stats.txt
-
