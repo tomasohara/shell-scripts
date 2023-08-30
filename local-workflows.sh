50a58f797a1cda5dcca40a12ed8630e14de8f419
Tom O'Hara <tohara@tohara.com>
2023-08-29 23:07:27 -0500

    updates header comments; removes commented out code

--- local-workflows.sh
+++ local-workflows.sh
@@ -1,9 +1,17 @@
 #!/bin/bash
 #
-# This script clones a GitHub repository, generates a pip requirements file,
+# This script uses GitHub local actions via act:
+#    https://github.com/nektos/act
+#
+# It also has support for using directly via docker. This includes
+# building the image and running via docker, done separately to support
+# inspection.
+# optio clones a GitHub repository, generates a pip requirements file,
 # builds a Docker image, and runs a Github Actions workflow locally.
 #
 # Note:
+# - There are some old options such as for cloning repository and
+#   generating requirements.
 # - When running under a Mac M1 the architecture needs to be specified to x64_64 (amd).
 #   This is a no-op otherwise (e.g., under Linux) as x64_64 is used by defauly.
 # - Selective shellcheck whitelisting:
@@ -33,10 +41,7 @@
 ## DEBUG: set -o xtrace
 REPO_URL="https://github.com/tomasohara/shell-scripts.git"
 REPO_DIR_NAME="shell-scripts"
-## OLD: IMAGE_NAME="local/test-act:latest"
 IMAGE_NAME="${REPO_DIR_NAME}-dev"
-## OLD: ACT_WORKFLOW="ubuntu-latest=local/test-act"
-## TODO:
 ACT_WORKFLOW="ubuntu:act-20.04"
 ACT_PULL="false"
 LOCAL_REPO_DIR="$PWD"
@@ -44,8 +49,8 @@ DEBUG_LEVEL="${DEBUG_LEVEL:-2}"
 GIT_BRANCH=${GIT_BRANCH:-""}
 BUILD_OPTS=${BUILD_OPTS:-}
 RUN_OPTS=${RUN_OPTS:-}
-# TODO3: put all up here fore clarity
-#  CLONE_REPO, AUTO_REQS, RUN_BUILD, BUILD_OPTS, RUN_WORKFLOW, RUN_OPTS, WORKFLOW_FILE
+# TODO3: put all env. init up here for clarity
+#   CLONE_REPO, AUTO_REQS, RUN_BUILD, BUILD_OPTS, RUN_WORKFLOW, RUN_OPTS, WORKFLOW_FILE
 if [ "$DEBUG_LEVEL" -gt 4 ]; then
     source "${TOM_BIN:-/home/tomohara/bin}/all-tomohara-aliases-etc.bash"
     trace-vars IMAGE_NAME LOCAL_REPO_DIR DEBUG_LEVEL GIT_BRANCH BUILD_OPTS
@@ -83,22 +88,16 @@ if [ "${RUN_BUILD:-1}" = "1" ]; then
     # note: maldito docker doesn't support --env for build, just run
     # Also, --build-arg misleading: see
     #    https://stackoverflow.com/questions/42297387/docker-build-with-build-arg-with-multiple-arguments
-    ## TODO: docker build --build-arg "DEBUG_LEVEL=$DEBUG_LEVEL" --build-arg "GIT_BRANCH=$GIT_BRANCH" --platform linux/x86_64 --tag "$IMAGE_NAME" .
     # shellcheck disable=SC2086
-    ## OLD: docker build --build-arg "GIT_BRANCH=$GIT_BRANCH" --platform linux/x86_64 $BUILD_OPTS --tag "name:$IMAGE_NAME" .
-    ## TEST
     docker build --build-arg "GIT_BRANCH=$GIT_BRANCH" --platform linux/x86_64 $BUILD_OPTS --tag my-shell-scripts .
 fi
 
 # Run the Github Actions workflow locally
-# TODO: get this to effing work [maldito act]
+# TODO1: get this to retain image (to allow for post-mortem review)
 if [ "${RUN_WORKFLOW:-1}" = "1" ]; then
     file="${WORKFLOW_FILE:-act.yml}"
-    ## TEST (Pass along debug level in environment)
     echo "Running Github Actions locally w/ $file"
     # shellcheck disable=SC2086
-    ## TODO: act --env DEBUG_LEVEL="$DEBUG_LEVEL" --container-architecture linux/amd64 --pull="$ACT_PULL" -platform "$ACT_WORKFLOW" --workflows ./.github/workflows/"$file" $RUN_OPTS
-    ## TEST: act --verbose --env DEBUG_LEVEL="$DEBUG_LEVEL" --container-architecture linux/amd64 --pull="$ACT_PULL" -platform my-shell-scripts --workflows ./.github/workflows/"$file" $RUN_OPTS
     act --verbose --env DEBUG_LEVEL="$DEBUG_LEVEL" --container-architecture linux/amd64 --pull="$ACT_PULL" --workflows ./.github/workflows/"$file" $RUN_OPTS
     # TODO: docker tag IMAGE-ID my-shell-scripts
     # EX (see above): docker tag $(docker images --quiet | head -1) my-shell-scripts
@@ -107,6 +106,5 @@ fi
 # Run via docker directly
 if [ "${RUN_DOCKER:-0}" = "1" ]; then
    echo "Running Tests via Docker"
-   ## OLD: docker run -it --env DEBUG_LEVEL="$DEBUG_LEVEL" --mount type=bind,source="$(pwd)",target=/home/shell-scripts shell-scripts-dev
    docker run -it --env DEBUG_LEVEL="$DEBUG_LEVEL" --mount type=bind,source="$(pwd)",target=/home/shell-scripts my-shell-scripts
 fi
