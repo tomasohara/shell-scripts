468817fbb64defb3862317d6cbd2f393b4746de5
Aviyan Acharya <avyn.xyz@gmail.com>
2023-10-03 22:37:42 +0545

    (EXPERIMENTAL/NOT-WORKING) Added logic for arg parsing options --first and --include

--- tests/automate_ipynb.py
+++ tests/automate_ipynb.py
@@ -44,11 +44,12 @@ VERBOSE = "verbose"
 
 ## Constants I (Initials for script)
 TL = debug.TL
-TESTFILE_URL = "http://127.0.0.1:8888/tree/tests/"
+TESTFILE_URL = "http://127.0.0.1:8888/tree/"
 JUPYTER_TOKEN = "111222"
 JUPYTER_EXTENSION = ".ipynb"
 NOBATSPP = "NOBATSPP"
 IPYNB_HELLO_WORLD_BASIC = "hello-world-basic.ipynb"
+COMMAND_RUN_JUPYTER = "run-jupyter-notebook"
 
 ## Constant II (Elements for Selenium Webdriver)
 SELENIUM_SLEEP_RERUN = 40
@@ -79,18 +80,31 @@ RANDOMIZE_TESTS = system.getenv_bool("RANDOMIZE_TESTS", False,
                         description="Randomized the testfile order during execution (Default: False)")
 DISABLE_SINGLE_INPUT = system.getenv_bool("DISABLE_SINGLE_INPUT", False,
                         description="Disables single testfile automation and allows all testfiles (if --include not used) (Default: False)")
+FORCE_RUN_JUPYTER = system.getenv_bool("FORCE_RUN_JUPYTER", True,
+                        description="Runs the command 'run-jupyter-notebook' when script executed (Default: True)")
 
 class AutomateIPYNB(Main):
     """Consists of functions for the automation of testfiles (.ipynb)"""
-    include_file = ""
-    first_n_testfile = 0
-    verbose = None
+    opt_include_file = ""
+    opt_first_n_testfile = 0
+    opt_verbose = None
+
+    def get_entered_text(self, label: str, default: str = "") -> str:
+        """
+        Return entered LABEL var/arg text by command-line or environment variable,
+        also can be specified a DEFAULT value
+        """
+        result = (self.get_parsed_argument(label=label.lower()) or
+                system.getenv_text(var=label.upper()))
+        result = result if result else default
+        debug.trace(7, f'automate_ipynb.get_entered_text(label={label}) => {result}')
+        return result
 
     def setup(self):
-        """Check results of command line processing"""        
-        self.include_file = self.get_parsed_argument(INCLUDE_TESTFILE, self.include_file)
-        self.first_n_testfile = self.get_parsed_argument(FIRST_N_TESTFILE, self.first_n_testfile)
-        self.verbose = self.get_parsed_argument(VERBOSE, self.verbose)
+        """Check results of command line processing"""
+        self.opt_include_file = self.get_entered_text(INCLUDE_TESTFILE, self.opt_include_file)
+        self.opt_first_n_testfile = int(self.get_entered_text(FIRST_N_TESTFILE, self.opt_first_n_testfile))
+        self.opt_verbose = self.get_parsed_option(VERBOSE, self.opt_verbose)
         debug.trace_object(5, self, label=f"{self.__class__.__name__} instance")
 
     def return_ipynb_url_array(self):
@@ -99,26 +113,36 @@ class AutomateIPYNB(Main):
         ipynb_files_filtered = [file for file in ipynb_files_all if not NOBATSPP in file]
         ipynb_url = []
 
-        # ipynb_files filtering according to ENV_OPTIONS and argparsing
+        # Create a copy of the original list
+        ipynb_files = ipynb_files_all.copy()
+
+        # Apply filtering conditions
+
         # A) SELECT_BATSPP selects each and every file
-        ipynb_files = ipynb_files_all.copy() if not SELECT_NOBATSPP else ipynb_files_filtered.copy()
+        if not SELECT_NOBATSPP:
+            ipynb_files = ipynb_files_filtered.copy()
 
-        # B) RANDOMIZE_TESTS randomizes the order of tests
+        # B) --first_n_testfiles option
+        if self.opt_first_n_testfile:
+            ipynb_files = ipynb_files[:self.opt_first_n_testfile]
+
+        # C) --include option
+        if self.opt_include_file:
+            # If an include file is specified, only include that file
+            ipynb_files = [self.opt_include_file]
+
+        # D) RANDOMIZE_TESTS randomizes the order of tests
         if RANDOMIZE_TESTS:
-            random.shuffle(ipynb_files)  # Shuffle the ipynb_files list if RANDOMIZE_TESTS is True
+            random.shuffle(ipynb_files)
 
-        # C) DISABLE_SINGLE_INPUT when false only selects one file for test (hello-world-basic.ipynb)
+        # E) DISABLE_SINGLE_INPUT when false only selects one file for test (hello-world-basic.ipynb)
         if not DISABLE_SINGLE_INPUT:
             if not RANDOMIZE_TESTS:
                 ipynb_files = [IPYNB_HELLO_WORLD_BASIC]
             else:
                 ipynb_files = [random.choice(ipynb_files)]
 
-        # D) --include option
-        # ipynb_files = [INCLUDED_FILE] if --include True else ipynb_files
-        # E) --first_n_testfiles option
-        # ipynb_files = ipynb_files(range_to_n) IF --first True else ipynb_files
-        
+        # Iterate through all files present in the array
         for file in ipynb_files:
             file_url = TESTFILE_URL + file
             print (file_url)
@@ -174,7 +198,7 @@ def main():
     """Entry point"""
     app = AutomateIPYNB(
         description = __doc__.format(script=gh.basename(__file__)),
-        skip_input = True,
+        skip_input = False,
         manual_input = True,
         auto_help = False,
         boolean_options=[
