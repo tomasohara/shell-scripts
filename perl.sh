efe9f7b4526810889d84c5e440393651d18b16cc
Tom O'Hara <tohara@tohara.com>
2023-10-03 17:48:56 -0500

    adds more debugging tracing

--- perl.sh
+++ perl.sh
@@ -3,7 +3,7 @@
 # Simple bash script wrapper around perl.
 #
 # Note:
-# - used to work around problem passing environment variables on command line.
+# - Used to work around problem passing environment variables on command line.
 #   For example,
 #      alias ps-sort-debug='DEBUG_LEVEL=5 perl.sh -Ssw ps_sort.perl'
 # - This is in support of DURING_ALIAS using in tomohara-aliases.bash.
@@ -11,6 +11,8 @@
 #
 
 ## DEBUG:
+## echo "in "${BASH_SOURCE[0]}""
+## echo '$0 "$@":' "$0" "$@"
 ## echo "DEBUG_LEVEL=$DEBUG_LEVEL"
 ## echo "DURING_ALIAS=$DURING_ALIAS"
 
