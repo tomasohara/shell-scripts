bf9718736c80bb9c8569e66e6d4cde6363aa4909
Aviyan Acharya <avyn.xyz@gmail.com>
2023-10-12 13:27:35 +0545

    (EXPERIMENTAL) Added tests for automate_ipynb.py

--- tests/test_automate_ipynb.py
+++ tests/test_automate_ipynb.py
@@ -10,22 +10,16 @@
 
 # Standard Packages
 import unittest
-import re
 
 # Installed Packages
 import pytest
 from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.common.alert import Alert
 
 # Local Modules
 from mezcla import debug
 from mezcla import glue_helpers as gh
 from mezcla import system
 from mezcla.my_regex import my_re
-
-# Script Import
 
 # Constants
 # TODO2: get OUTPUT_DIR from THE_MODULE.OUTPUT_DIR
@@ -37,7 +31,7 @@ debug.assertion(TEST_DIR.endswith("tests"))
 
 class TestAutomateIPYNB(unittest.TestCase):
     """Class for testcase definition"""
-    script = gh.resolve_path("automate_batspp_indev.py")
+    script = gh.resolve_path("automate_batspp.py")
     temp = None
 
     def run_script(self, arguments):
@@ -51,10 +45,34 @@ class TestAutomateIPYNB(unittest.TestCase):
     def test_script_help(self):
         """Make sure script usage shown with --help"""
         result = self.run_script("--help")
-        assert(my_re.search(r"Test.*Automate.*Bash", result))
+        assert(my_re.search(r"automate", result))
+
+    def test_env_opt_select_nobatspp(self):
+        """Tests environment option: SELECT_NOBATSPP"""
+        debug.trace(4, f"TestBatsppReport.test_env_opt_select_nobatspp(); self={self}")
+
+    @pytest.mark.xfail
+    def test_include_option(self):
+        """Tests include option from script"""
+        debug.trace(4, f"TestBatsppReport.test_include_option(); self={self}")
+        testfile_calculator = "alias-calculator-commands"
+        self.temp = gh.create_temp_file(contents="")
+        self.run_script(f"--include {testfile_calculator} > {self.temp}")
+        output = gh.read_lines(self.temp)
+        assert testfile_calculator in output
+
+    @pytest.mark.xfail
+    def test_first_option(self):
+        """Tests first option from script"""
+        debug.trace(4, f"TestBatsppReport.test_first_option(); self={self}")
+        first_value = 2
+        self.temp = gh.create_temp_file(contents="")
+        self.run_script(f"--first {first_value} > {self.temp}")
+        output = gh.read_lines(self.temp)
+        urls = ["http://127.0.0.1:8888/tree/trace-python-commands.ipynb", "http://127.0.0.1:8888/tree/trace-line-words-commands.ipynb"]
+        assertion_case = (output[0] == urls[0] and output[1] == urls[1] and output[2] == "")
+        assert assertion_case
 
 
 if __name__ == "__main__":
     unittest.main()
-
-     
