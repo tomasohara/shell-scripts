d098397ddbad25453d4aaa35a22863beb68f10b8
Tom O'Hara <tohara@tohara.com>
2024-01-11 20:59:48 -0600

    fixes eval_condition import and script_module init; adds test_stdin

--- tests/tests/test_eval_condition.py
+++ tests/tests/test_eval_condition.py
@@ -11,17 +11,32 @@ import pytest
 # Local packages
 from mezcla.unittest_wrapper import TestWrapper
 from mezcla import debug
+from mezcla import system
 
 # Note: Two references are used for the module to be tested:
-#    THE_MODULE:	    global module object
-## TODO: template => new name
-import eval_condition as THE_MODULE
-
+#    THE_MODULE:	                global module object
+#    TestIt.script_module:              path to file
+try:
+    import tests.eval_condition as THE_MODULE
+except:
+    system.print_exception_info("loading eval_condition.py")
+    THE_MODULE = None
 
 class TestEvalCondition(TestWrapper):
     """Class for testcase definition"""
-    script_file = TestWrapper.get_module_file_path(__file__)
-    script_module = TestWrapper.get_testing_module_name(__file__)
+    script_module = TestWrapper.get_testing_module_name(__file__, THE_MODULE)
+
+    @pytest.mark.xfail                   # TODO: remove xfail
+    def test_stdin(self):
+        """Make sure special variable stdin used properly in test"""
+        debug.trace(4, "test_stdin()")
+        system.write_lines(self.temp_file, ["l1", "l2"])
+        # note: need to put expression in quotes due to special shell tokens (e.g., '<')
+        output = self.run_script("'1 < len(stdin) < 7'",
+                                 uses_stdin=True, data_file=self.temp_file)
+        assert output == "True"
+        output = self.run_script("'stdin == \"l1 l2\"'", data_file=self.temp_file)
+        assert output == "False"
 
     def test_eval_true(self):
         """Make sure eval_condition() returns True"""
