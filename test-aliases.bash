4e288979e87033340e3358afebeba0582f223609
Geremias Baudino <61209297+gbaudino@users.noreply.github.com>
2024-01-17 20:10:14 -0300

    Move eval-condition alias to a more specific aliases file (test-aliases.bash).

---
+++ test-aliases.bash
@@ -0,0 +1,10 @@
+# test-aliases.bash: python aliases for testing tools
+#
+# usage:
+#    source ~/bin/test-aliases.bash
+#
+# testing tools:
+#   eval-condition: evaluates a condition and optionally executes a command if the condition is true
+
+
+alias eval-condition='python eval_condition.py'
