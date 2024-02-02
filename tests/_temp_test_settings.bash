dad9b9d3d51ed45360ee546ef5034fae3d723732
Scrappy Cito <scrappycito@gmail.com>
2024-02-02 01:19:37 -0600

    adds USE_SSH_AUTH; adds commented out REMAP_TMP; adds another warning about not merging into main; refines comments

--- tests/_temp_test_settings.bash
+++ tests/_temp_test_settings.bash
@@ -14,7 +14,7 @@
 #
 #    export TEST_REGEX="tips|README"    # run tests with tips or README in file
 #
-export DEBUG_LEVEL=5
+export DEBUG_LEVEL=4               # use verbose tracing
 
 # HACK: settings for testing BatsPP archive creation
 ## TEMP: export UNDER_DOCKER=1
@@ -22,10 +22,17 @@ export DEBUG_LEVEL=5
 # Regular settings
 # Note: Archiving output will only be done if running under docker;
 # also, scp will be skipped. It is used for debugging Github Actions
-export ARCHIVE_OUTPUT=1
-export SCP_OUTPUT=0
+export ARCHIVE_OUTPUT=1            # create output archive in repo root under docker
+export SCP_OUTPUT=0                # don't copy results to server via scp
+export USE_SSH_AUTH=1              # use ssh w/ scrappycito.pem for git updates
 
 # Temporary settings
+#
 # Note: set regex for debugging specific tests. You can check it in to
-# test Github actions, but 
-# TEMP: export TEST_REGEX="tips|entropy|testing-support-script-tests|eval-condition|sanity"
+# test Github actions, but again *** don't merge into main ***.
+#
+## export TEST_REGEX="tips|entropy|testing-support-script-tests|eval-condition|sanity"
+##
+## export REMAP_TMP=1                 # all /tmp files => $BATSPP_TEMP (see summary_stats.bash)
+##
+## export SHOW_FAILURE_CONTEXT=0    # hide redundant error report (see batspp_report.py)
