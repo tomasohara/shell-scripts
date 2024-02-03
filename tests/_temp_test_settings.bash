4c7131e010b31945c1ac5a317276a3ece361055f
Tom O'Hara <tomasohara@gmail.com>
2024-02-03 16:40:41 -0600

    only makes settings if unless under testing VM

--- tests/_temp_test_settings.bash
+++ tests/_temp_test_settings.bash
@@ -14,7 +14,7 @@
 #
 #    export TEST_REGEX="tips|README"    # run tests with tips or README in file
 #
-export DEBUG_LEVEL=4               # use verbose tracing
+## export DEBUG_LEVEL=4               # use verbose tracing
 
 # HACK: settings for testing BatsPP archive creation
 ## TEMP: export UNDER_DOCKER=1
@@ -22,17 +22,24 @@ export DEBUG_LEVEL=4               # use verbose tracing
 # Regular settings
 # Note: Archiving output will only be done if running under docker;
 # also, scp will be skipped. It is used for debugging Github Actions
-export ARCHIVE_OUTPUT=1            # create output archive in repo root under docker
-export SCP_OUTPUT=0                # don't copy results to server via scp
-export USE_SSH_AUTH=1              # use ssh w/ scrappycito.pem for git updates
+## export ARCHIVE_OUTPUT=1            # create output archive in repo root under docker
+## export SCP_OUTPUT=0                # don't copy results to server via scp
+## export USE_SSH_AUTH=0              # use ssh w/ scrappycito.pem for git updates
+
+# Override settings if under tetsing VM
+# note: most settings off so user can override when running locally,
+# but, it is awkward to do so for docker or Github runner jobs.
+if [ "$DEPLOYMENT_BASEPATH" == "/opt/runner" ]; then
+    export ARCHIVE_OUTPUT=1
+    export USE_SSH_AUTH=1
+    export DEBUG_LEVEL=5
+fi
 
 # Temporary settings
 #
 # Note: set regex for debugging specific tests. You can check it in to
 # test Github actions, but again *** don't merge into main ***.
 #
-## export TEST_REGEX="tips|entropy|testing-support-script-tests|eval-condition|sanity"
-##
-## export REMAP_TMP=1                 # all /tmp files => $BATSPP_TEMP (see summary_stats.bash)
-##
+## export TEST_REGEX="git|tips|entropy|testing-support-script-tests|eval-condition|sanity"
+## export REMAP_TMP=1               # all /tmp files => $BATSPP_TEMP (see summary_stats.bash)
 ## export SHOW_FAILURE_CONTEXT=0    # hide redundant error report (see batspp_report.py)
