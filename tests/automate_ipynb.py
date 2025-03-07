22a470465ac3f6c39a3ccdaaca9337f3cde59bad
Scrappy Cito <scrappycito@gmail.com>
2025-03-06 21:17:13 -0600

    traps automation calls; uses filename unless -; adds pause; reworks driver init; uses local file unless http; adds sanity check

--- tests/automate_ipynb.py
+++ tests/automate_ipynb.py
@@ -50,6 +50,7 @@ JUPYTER_EXTENSION = ".ipynb"
 NOBATSPP = "NOBATSPP"
 IPYNB_HELLO_WORLD_BASIC = "hello-world-basic.ipynb"
 COMMAND_RUN_JUPYTER = "run-jupyter-notebook"
+debug.assertion(TESTFILE_URL.endswith("/"))
 
 ## Constant II (Elements for Selenium Webdriver)
 SELENIUM_SLEEP_RERUN = 40
@@ -82,12 +83,15 @@ DISABLE_SINGLE_INPUT = system.getenv_bool("DISABLE_SINGLE_INPUT", False,
                         description="Disables single testfile automation and allows all testfiles (if --include not used) (Default: False)")
 FORCE_RUN_JUPYTER = system.getenv_bool("FORCE_RUN_JUPYTER", True,
                         description="Runs the command 'run-jupyter-notebook' when script executed (Default: True)")
+END_PAUSE = system.getenv_float("END_PAUSE", 0,
+                                "Number of seconds to pause after running")
 
 class AutomateIPYNB(Main):
     """Consists of functions for the automation of testfiles (.ipynb)"""
     opt_include_file = ""
     opt_first_n_testfile = 0
     opt_verbose = None
+    driver = None
 
     def get_entered_text(self, label: str, default: str = "") -> str:
         """
@@ -105,6 +109,7 @@ class AutomateIPYNB(Main):
         self.opt_include_file = self.get_entered_text(INCLUDE_TESTFILE, self.opt_include_file)
         self.opt_first_n_testfile = int(self.get_entered_text(FIRST_N_TESTFILE, self.opt_first_n_testfile))
         self.opt_verbose = self.get_parsed_option(VERBOSE, self.opt_verbose)
+        self.driver = webdriver.Firefox() if USE_FIREFOX else webdriver.Chrome()
         debug.trace_object(5, self, label=f"{self.__class__.__name__} instance")
 
     def return_ipynb_url_array(self):
@@ -151,8 +156,13 @@ class AutomateIPYNB(Main):
         print(f"SELECT_NOBATSPP: {SELECT_NOBATSPP}")
         print("\nDuration for each testfiles (in seconds):")
         for url in url_arr:
+            if not url.startswith("http"):
+                url = TESTFILE_URL + url
             start_time = time.time()
-            driver = webdriver.Firefox() if USE_FIREFOX else webdriver.Chrome()
+
+            ## OLD: driver = webdriver.Firefox() if USE_FIREFOX else webdriver.Chrome()
+            driver = self.driver
+            debug.trace_expr(5, url)
             driver.get(url)
             time.sleep(1)
             if JUPYTER_PASSWORD:
@@ -162,31 +172,39 @@ class AutomateIPYNB(Main):
                 token_input_submit.click()
 
             # TODO: In the case of Invalid Credentials
+            try:
+                time.sleep(1)
+                driver.find_element(By.ID, ID_KERNELLINK).click()
+                time.sleep(3)
+                driver.find_element(By.ID, ID_RESTART_RUN_ALL).click()
+                time.sleep(3)
+                driver.find_element(By.XPATH, XPATH_RESTART_RUN_ALL_WARNING_RED).click()
+                time.sleep(AUTOMATION_DURATION_RERUN)
+                driver.find_element(By.ID, ID_FILELINK).click()
+                time.sleep(2)
+                driver.find_element(By.ID, ID_SAVE_CHECKPOINT).click()
+                time.sleep(2)
+                driver.find_element(By.ID, ID_FILELINK).click()
+                time.sleep(2)
+                driver.find_element(By.ID, ID_CLOSE_AND_HALT).click()
+                time.sleep(2)
+                ## TODO: put quit in new wrap_up method
+                ## OLD: driver.quit()
+            except:
+                system.print_exception_info(f"navigating url {url}")
 
-            time.sleep(1)
-            driver.find_element(By.ID, ID_KERNELLINK).click()
-            time.sleep(3)
-            driver.find_element(By.ID, ID_RESTART_RUN_ALL).click()
-            time.sleep(3)
-            driver.find_element(By.XPATH, XPATH_RESTART_RUN_ALL_WARNING_RED).click()
-            time.sleep(AUTOMATION_DURATION_RERUN)
-            driver.find_element(By.ID, ID_FILELINK).click()
-            time.sleep(2)
-            driver.find_element(By.ID, ID_SAVE_CHECKPOINT).click()
-            time.sleep(2)
-            driver.find_element(By.ID, ID_FILELINK).click()
-            time.sleep(2)
-            driver.find_element(By.ID, ID_CLOSE_AND_HALT).click()
-            time.sleep(2)
-            driver.quit()
             end_time = time.time()
             print(f"#{test_count}. {url.split('/')[-1]}: {round(end_time - start_time, 2)}")
             test_count += 1
+            if END_PAUSE:
+                time.sleep(END_PAUSE)
 
     def run_main_step(self):
         """Process main script"""
         start_time_main = time.time()
-        self.automate_testfile(self.return_ipynb_url_array())
+        ## OLD: self.automate_testfile(self.return_ipynb_url_array())
+        test_files = [self.filename] if (self.filename != "-") else self.return_ipynb_url_array()
+        self.automate_testfile(test_files)
         print(f"\nTotal Time (including pauses): {round(time.time() - start_time_main, 2)} sec\n")
 
 def main():
