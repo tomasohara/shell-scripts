363a0d5d7e3dfb2234ee51e8688461142773beb1
Tom O'Hara <tohara@tohara.com>
2026-03-04 22:17:11 -0500

    adds ## OLD: trace to do_diff.bash and fixes missing 3.12 in github.yml matrix

--- do_diff.bash
+++ do_diff.bash
@@ -100,6 +100,7 @@ if [ -z "$2" ]; then
     echo "git ls-tree -r --name-only HEAD | xargs -I '{}' $script --no-pattern '{}' ~/repo-main/'{}' > _main-diff-tracked.log 2>&1"
     
     echo ""
+    ## OLD: echo "$script" '--match-dot-files ".*bash*" .. > _bash-diff.list 2>&1'
     echo "$script" '--match-dot-files ".*bash*" .. > _bash-diff-$(todays-date).list 2>&1'
     ## TODO: add notes on aliases used in script usages (i.e., here and elsewhere)
     echo ""
