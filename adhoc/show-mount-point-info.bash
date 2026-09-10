6320abc298a1156f27c598d961539ab1cf294713
Tom O'Hara <tohara@tohara.com>
2026-09-10 14:02:01 -0400

    allows check for specified mount point; adds usage and illustrates /etc/fstab format

--- adhoc/show-mount-point-info.bash
+++ adhoc/show-mount-point-info.bash
@@ -18,12 +18,49 @@
 #    /mnt/micro-sd-1tb            n/a                   
 #    /mnt/wd6tbp1ext              n/a                   
 #    /mnt/wd6tbp2ntfs             n/a                   
-#    /mnt/micro-sd-256gb          n/a                   
+#    /mnt/micro-sd-256gb          n/a
+#...............................................................................
+# Format of /etc/fstab:
+#   <file system> <mount point>   <type>  <options>       <dump>  <pass>
+# Examples:
+#   /dev/disk/by-uuid/c18192ff-4585-4df7-b891-8654be8eae49 / ext4 defaults 0 1
+#   /dev/disk/by-uuid/620B-BDEE /boot/efi vfat defaults 0 2
+#   /swapfile	none	swap	sw	0	0
+#   /dev/mmcblk0p1 /mnt/resmed vfat nosuid,nodev,nofail,noauto,x-gvfs-show,uid=1000,gid=1000 0 0
+#
+## UPDATE 10 Sep 26: allows check for specified mount point
+
 #-------------------------------------------------------------------------------
 # TODO:
 # - find happy medium between Perl and Python without using awk
 #    
-    
+
+function show-mount-info {
+    local mp="$1"
+    if findmnt -rn "$mp" >/dev/null 2>&1; then
+        df -h --output=target,source,size,used,avail,pcent "$mp" \
+            | awk 'NR==2 {
+                          printf "%-28s %-18s %6s %6s %6s %4s\n",
+                                       $1,$2,$3,$4,$5,$6
+                      }'               
+    else
+        printf "%-28s %-18s %6s %6s %6s %4s\n" \
+               "$mp" "n/a" "" "" "" ""
+    fi
+}
+
+
+# Display command-line usage
+# TODO2: add option to hide /mnt/fstab entries
+if [ "$1" == "--help" ]; then
+    script=$(basename "$0")
+    echo "usage: $script [mount-point ...] [-]"
+    echo ""
+    echo "Examples:"
+    echo ""
+    echo "$0 /media/tomohara/UBUNTU_24_0"
+    exit
+fi
 
 printf "%-28s %-18s %6s %6s %6s %4s\n" \
        "Mount" "Filesystem" "Size" "Used" "Avail" "Use%"
@@ -33,14 +70,9 @@ awk '!/^#/ && NF {print $2}' /etc/fstab |
         if [ "$mp" == "none" ]; then
             continue
         fi
-        if findmnt -rn "$mp" >/dev/null 2>&1; then
-            df -h --output=target,source,size,used,avail,pcent "$mp" \
-                | awk 'NR==2 {
-                          printf "%-28s %-18s %6s %6s %6s %4s\n",
-                                       $1,$2,$3,$4,$5,$6
-                      }'               
-        else
-            printf "%-28s %-18s %6s %6s %6s %4s\n" \
-                   "$mp" "n/a" "" "" "" ""
-        fi
+        show-mount-info "$mp"
     done
+
+for mp in "$@"; do
+    show-mount-info "$mp"
+done
